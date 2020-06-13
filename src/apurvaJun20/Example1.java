998c231a1537913de16975f868533dc9dc230138
Maulik <maulik.kanani@sensiaglobal.com>
2020-06-13 23:02:02 +0530

    Uploading project

---
+++ src/apurvaJun20/Example1.java
@@ -0,0 +1,20 @@
+package apurvaJun20;
+
+public class Example1 {
+	static int rno;
+	String name;
+	
+	void displayData() {
+		System.out.println(rno + " " + name);
+	}
+	
+	public static void main(String[] args) {
+		Example1 example1 = new Example1(); // name="M" [I] // rno=10 [S]
+		example1.name = "Maulik";
+		
+		Example1 example2 = new Example1(); // name = "K" 
+		example2.name = "Krishna";
+		
+		example1.displayData(); //
+	}
+}
