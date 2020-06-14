4d7daa359f073c3b4f4a515b32953d9434bad8c7
Mahesh.Kumavat <mkumavat31@gmail.com>
2020-06-14 12:43:21 +0530

    First Sample git commit

---
+++ src/maheshKJun20/Test.java
@@ -0,0 +1,16 @@
+package maheshKJun20;
+
+public class Test {
+	
+	void display() {
+		
+		System.out.println("First sample git commit");
+	}
+	
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.display();
+	}
+
+}
+
