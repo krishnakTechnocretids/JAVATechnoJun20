25006ea613fbf99e396919d59673c23e8b15406f
Apurva <apurvagupta1410@gmail.com>
2020-06-14 13:47:34 +0530

    Update Name

--- src/apurvaJun20/Example1.java
+++ src/apurvaJun20/Example1.java
@@ -10,10 +10,10 @@ public class Example1 {
 	
 	public static void main(String[] args) {
 		Example1 example1 = new Example1(); // name="M" [I] // rno=10 [S]
-		example1.name = "Maulik";
+		example1.name = "Apurva";
 		
 		Example1 example2 = new Example1(); // name = "K" 
-		example2.name = "Krishna";
+		example2.name = "Shiro";
 		
 		example1.displayData(); //
 	}
