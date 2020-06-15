ea8daa04bede4ff689dc3466889cbb8dd83914c2
Amita Rout <amitarout11@gmail.com>
2020-06-14 23:07:59 -0400

    Demo1 file pushed for review

---
+++ src/amitaRJun20/Demo1.java
@@ -0,0 +1,15 @@
+package amitaRJun20;
+
+public class Demo1 {
+	
+	void Display()	{
+		String name="Technocredits";
+		System.out.println(name);
+	}
+	
+	public static void main(String[] args) {
+		Demo1 demo=new Demo1();
+		demo.Display();
+		
+	}
+}
