6185dce5de0fe98fa6c93aafc0bc8b4454ca7159
Harshad Bhore <hsb9595@gmail.com>
2020-06-15 08:41:14 +0530

    Demo Assignment to print number

---
+++ src/harshadBJun20/Demo.java
@@ -0,0 +1,15 @@
+package harshadBJun20;
+
+public class Demo {
+	
+	int Number=10;
+	
+	void Display() {
+		System.out.println("Number in Demo class: " +Number);
+	}
+	
+	public static void main(String[] args) {
+		Demo demo = new Demo();
+		demo.Display();
+	}
+}
