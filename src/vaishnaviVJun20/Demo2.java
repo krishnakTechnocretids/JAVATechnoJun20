66bb1e06487c3e891b41549adf5ff52d219fac5a
Vaishnavi Vaidya <vaishnavivaidya06@gmail.com>
2020-06-15 08:40:48 +0530

    Added demo2

---
+++ src/vaishnaviVJun20/Demo2.java
@@ -0,0 +1,14 @@
+package vaishnaviVJun20;
+
+public class Demo2 {
+
+		void Display()
+		{
+			System.out.println("My name is vaishnavi");
+		}
+		public static void main(String[] args) {
+			Demo demo = new Demo();
+			demo.Display();
+		}
+
+}
