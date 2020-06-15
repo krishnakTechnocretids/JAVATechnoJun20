7f5862de2912efcf238349608e499f1af587ff67
VireshJ <jangamviresh@yahoo.com>
2020-06-15 08:39:48 +0530

    please audit the changes

--- src/vireshJJun20/Test.java
+++ src/vireshJJun20/Test.java
@@ -1,9 +1,14 @@
 package vireshJJun20;
 
 public class Test {
-
-	public static void main(String[] args) {
+	
+	void Display() {
 		System.out.println("Viresh");
+	}
+	public static void main(String[] args) {
+		Test test= new Test();
+		
+		test.display();
 	}
 
 }
