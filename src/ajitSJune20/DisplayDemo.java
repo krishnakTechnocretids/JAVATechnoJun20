ae312ddf2d47f2c2bfdc2457f02671b466dc25cc
Ajit Singh <hi2ajitkumar@gmail.com>
2020-06-15 08:37:38 +0530

    Committed new code within branch (Ajit_Demo_Assignment_2)

---
+++ src/ajitSJune20/DisplayDemo.java
@@ -0,0 +1,16 @@
+package ajitSJune20;
+
+public class DisplayDemo {
+
+	int d;
+	void Display() {
+		System.out.println("Naming Convension of display method is wrong");
+	}
+
+	public static void main(String[] args) {
+		DisplayDemo displayDemo = new DisplayDemo();
+		displayDemo.Display();
+
+	}
+
+}
