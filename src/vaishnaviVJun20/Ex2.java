678f9cb72eae362eef9fdca6493ba9f56c00c7cc
Vaishnavi Vaidya <vaishnavivaidya06@gmail.com>
2020-06-15 08:57:33 +0530

    Done

---
+++ src/vaishnaviVJun20/Ex2.java
@@ -0,0 +1,16 @@
+package vaishnaviVJun20;
+
+public class Ex2 {
+	
+	void Display()
+	{
+		System.out.println("Viahsnavi");
+	}
+
+	public static void main(String[] args) {
+		Ex2 ex2 = new Ex2();
+		ex2.Display();
+
+	}
+
+}
