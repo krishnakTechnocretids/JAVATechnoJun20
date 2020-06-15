b441d79044dc069359b79e737e089638c0805325
Deavina Sharma <deavina.sharma@gmail.com>
2020-06-15 08:34:03 +0530

    new file

---
+++ src/deavinaSJun20/TestExample.java
@@ -0,0 +1,14 @@
+package deavinaSJun20;
+
+public class TestExample {
+
+	void Display() {
+		
+		System.out.println("print");
+	}
+	public static void main(String[] args) {
+		TestExample t1=new TestExample();
+		t1.Display();
+	}
+	
+}
