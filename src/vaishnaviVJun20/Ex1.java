561736c0eadbe6fc94842930a12a939fab438271
Vaishnavi Vaidya <vaishnavivaidya06@gmail.com>
2020-06-15 08:45:44 +0530

    Assignment done

---
+++ src/vaishnaviVJun20/Ex1.java
@@ -0,0 +1,16 @@
+package vaishnaviVJun20;
+
+public class Ex1 {
+
+	void Display()
+	{
+		System.out.println("My name is vaishnavi");
+	}
+	public static void main(String[] args) {
+		Ex1 ex1 = new Ex1();
+		ex1.Display();
+	}
+		
+
+}
+
