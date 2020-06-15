263971821ea9f20aa203dd50b729366421c139ad
Archana Nawale <nawalearchana0@gmail.com>
2020-06-15 09:11:12 +0530

    Updated comment for Example2

--- src/archanaNJun20/Example2.java
+++ src/archanaNJun20/Example2.java
@@ -5,17 +5,14 @@ public class Example2 {
 		static int rno;
 		String name;
 		
-		void DisplayData() {
+		void displayData() {
 			System.out.println(rno + " " + name);
 		}
-		
 		public static void main(String[] args) {
 			Example1 example1 = new Example1(); // name="M" [I] // rno=10 [S]
 			example1.name = "Maulik";
-		
 			Example1 example2 = new Example1(); // name = "K" 
 			example2.name = "Krishna";
-			
 			example1.displayData(); //
 		}
 }
--- src/archanaNJun20/Test.java
+++ src/archanaNJun20/Test.java
@@ -4,6 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Archana");
+		int marks=29;
+		if(marks > 29);
+		   System.out.print("PASS ");
+		System.out.println("RANK");
 	}
 }
