7b830b97fe8f7b7c20e42fdabc3b87ee7c8e50a5
Karan Kumar Das <karankumar15das@gmail.com>
2020-06-15 08:37:10 +0530

    Assginment completed

---
+++ src/kARAN1595/Example2.java
@@ -0,0 +1,15 @@
+package kARAN1595;
+
+public class Example2 {
+
+	void Display(){
+	System.out.println("Karan Here");	
+	}
+	
+	public static void main(String[] args) {
+		Example2 ex2 =new Example2();
+		ex2.Display();
+
+	}
+
+}
