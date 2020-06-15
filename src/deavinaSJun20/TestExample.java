2e0881c4445fc657c5c6cbfbf8dc03f2b4312343
Deavina Sharma <deavina.sharma@gmail.com>
2020-06-15 08:48:32 +0530

    Updated File

--- src/deavinaSJun20/TestExample.java
+++ src/deavinaSJun20/TestExample.java
@@ -2,13 +2,11 @@ package deavinaSJun20;
 
 public class TestExample {
 
-	void Display() {
-		
+	void display() {
 		System.out.println("print");
 	}
 	public static void main(String[] args) {
 		TestExample t1=new TestExample();
-		t1.Display();
+		t1.display();
 	}
-	
 }
