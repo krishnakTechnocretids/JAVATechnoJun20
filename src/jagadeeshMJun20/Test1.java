053c935d144a90b6e50ea72acfb1355ff2811d9e
Jagadeesh Mangipudi <jagadeesh.mangipudi412@gmail.com>
2020-06-15 21:05:47 +0530

    New and modified codes

---
+++ src/jagadeeshMJun20/Test1.java
@@ -0,0 +1,16 @@
+package jagadeeshMJun20;
+
+public class Test1 {
+
+	void display() {
+		System.out.println("To learn Git process");
+	}
+	public static void main(String[] args) {
+		Test1 test1 = new Test1();
+		test1.display();
+		
+				
+
+	}
+
+}
