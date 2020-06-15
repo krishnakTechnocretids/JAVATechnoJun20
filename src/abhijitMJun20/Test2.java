df48b210437130114e7965fd4760ad7926054d59
Abhijit Mali <abhijitmali94@gmail.com>
2020-06-15 12:21:04 +0530

    Space removed.

--- src/abhijitMJun20/Test2.java
+++ src/abhijitMJun20/Test2.java
@@ -1,10 +1,7 @@
 package abhijitMJun20;
 
 public class Test2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
       System.out.println("Git Testing Part 2");
 	}
-
 }
