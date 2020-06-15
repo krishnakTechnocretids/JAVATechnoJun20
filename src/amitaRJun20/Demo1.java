6b3dc8f4fc3591de1938b994ce902619f7e2dec1
Amita Rout <amitarout11@gmail.com>
2020-06-14 23:21:32 -0400

    Incorporated the changes in Demo1

--- src/amitaRJun20/Demo1.java
+++ src/amitaRJun20/Demo1.java
@@ -5,11 +5,9 @@ public class Demo1 {
 	void Display()	{
 		String name="Technocredits";
 		System.out.println(name);
-	}
-	
+	}	
 	public static void main(String[] args) {
 		Demo1 demo=new Demo1();
-		demo.Display();
-		
+		demo.Display();		
 	}
 }
