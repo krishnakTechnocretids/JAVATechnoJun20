bebaf4f4bf9f5ee0960036f3eefc20b97dce2f3d
Anshu Kumari <anshu.v.kumari02@gmail.com>
2020-06-15 09:10:15 +0530

    Suggested Change Done

--- src/anshuKJun20/Demo.java
+++ src/anshuKJun20/Demo.java
@@ -1,8 +1,7 @@
 package anshuKJun20;
 
 public class Demo {
-public static void main(String[] args) {
+public static void main(String[] args){
 	System.out.println("Anshu Assignment @");
-}
-
+  }
 }
