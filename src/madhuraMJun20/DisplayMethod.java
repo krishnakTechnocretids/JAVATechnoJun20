2c0e090edbcb85804c58acdf28f1d23b3e509563
Madhura Mulay <madhurah02@gmail.com>
2020-06-15 08:38:10 +0530

    code is commited

---
+++ src/madhuraMJun20/DisplayMethod.java
@@ -0,0 +1,14 @@
+package madhuraMJun20;
+
+public class DisplayMethod {
+	
+	void Display() {
+		System.out.println("Madhura");
+	}
+	
+	public static void main(String [] args) {
+		DisplayMethod displayMethod = new DisplayMethod();
+		displayMethod.Display();
+	}
+
+}
