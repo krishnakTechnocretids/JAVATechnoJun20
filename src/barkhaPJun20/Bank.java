2821b9ec7cfc01b2ee2a5e506dc840cafac5d11a
Barkha Patle <barkhapatle01@gmail.com>
2020-06-16 17:55:22 +0530

    Barkha_Assignment_8_Done

---
+++ src/barkhaPJun20/Bank.java
@@ -0,0 +1,91 @@
+package barkhaPJun20;
+
+public class Bank {
+	int debitCount=0;
+	int creditCount=0;
+	int balanceCount=0;
+	
+	static int overallDebitCount=0;
+	static int overallCreditCount=0;
+	static int overallBalanceCount=0;
+	
+	int initialAmnt;
+	int debitAmnt;
+	int creditAmnt;
+	int currentBalance;
+	
+	int user;
+	
+	void setInitialAmnt(int uID,int Amnt){
+		user=uID;
+		currentBalance=Amnt;
+	}
+	
+	void debitAmount( int dAmnt){
+		debitAmnt=dAmnt;
+		currentBalance=currentBalance-debitAmnt;
+		debitCount++;
+		overallDebitCount++;
+	}	
+	void creditAmount( int cAmnt){
+		creditAmnt=cAmnt;
+		currentBalance=currentBalance+creditAmnt;
+		creditCount++;
+		overallCreditCount++;
+	}
+	void printBalance(){
+		System.out.println();
+		System.out.println("Final Amount is: "+currentBalance +" for customer "  +user);
+		balanceCount++;
+		overallBalanceCount++; 
+	}
+	void individualTransactionSummary(){
+		System.out.println();
+		if (user==1)
+			 System.out.println("Customer1 Transaction Summary: Debit "  +debitCount +" times, Credit " +creditCount +" times, Print balance "+balanceCount);
+		if (user==2)
+			 System.out.println("Customer2 Transaction Summary: Debit "  +debitCount +" times, Credit " +creditCount +" times, Print balance "+balanceCount);
+	}	
+	void allTransactionSummary(){
+		System.out.println();
+		System.out.println("Overall Transaction by both users");
+		System.out.println ("Overall Credit done " +overallCreditCount);
+		System.out.println ("Overall Debit done" +overallDebitCount);
+		System.out.println ("Overall Print Balance done " +overallBalanceCount);
+	}
+	public static void main(String[] args){
+		
+		Bank bank1=new Bank();
+		bank1.setInitialAmnt(1,1000);
+		bank1.debitAmount(50);
+		bank1.creditAmount(500);
+		bank1.creditAmount(300);
+		bank1.printBalance();
+		bank1.individualTransactionSummary();
+		
+		
+		Bank bank2=new Bank();
+		bank2.setInitialAmnt(2,2000);
+		bank2.debitAmount(190);
+		bank2.debitAmount(75);
+		bank2.creditAmount(700);
+		bank2.creditAmount(70);
+		bank2.creditAmount(200);
+		bank2.creditAmount(100);
+		bank2.creditAmount(90);
+		bank2.printBalance();
+		bank2.individualTransactionSummary();
+		
+		
+		bank2.allTransactionSummary();
+		
+	}
+
+}
+
+		
+	
+	
+	
+	
+	
