1927ba84281e6b0c1633d649353755e3238402ad
Aavruti Dalmia <aavruti14@gmail.com>
2020-06-16 23:25:02 +0530

    Modified changes as per review for Banking System Assignment

--- src/aavrutiDJun20/BankingSystem.java
+++ src/aavrutiDJun20/BankingSystem.java
@@ -2,52 +2,55 @@ package aavrutiDJun20;
 
 public class BankingSystem {
 
-	int user2printBalCount = 0;
-	int currentBalance = 0;
-	int debitCount = 0;
-	int creditCount = 0;
-	int printBalanceCount = 0;
+	int user2printBalCount;
+	int currentBalance;
+	int debitCount;
+	int creditCount;
+	int printBalanceCount;
 	static int allUsersDebitCnt;
 	static int allUsersCreditCnt;
 	static int allUsersPrintBalance;
 	
-	int amountCreditedByUser(int userCreditAmount) {
-		if(userCreditAmount>=0) {
+	//Amount credited to user, adding amount in balance and printing the amount
+	void amountCreditedByUser(int userCreditAmount) {
+		if(userCreditAmount>0) {
 			currentBalance = currentBalance + userCreditAmount;
 			System.out.println("Credit Amount : " + userCreditAmount );
 			allUsersCreditCnt++;
-			return ++creditCount;
+			creditCount++;
 		}
 		else {
 			System.out.println("Please enter credit amount more than 0");
-			return creditCount;
 		}
 	}
 	
-	int amountDebitedByUser(int userDebitAmount) {
+	//amount taken out from the account, debit amount from the balance and printing the amount
+	void amountDebitedByUser(int userDebitAmount) {
 		if(userDebitAmount>=0 && userDebitAmount<=currentBalance) {
 			allUsersDebitCnt++;
 			System.out.println("Debit Amount  : " + userDebitAmount );
 			currentBalance = currentBalance - userDebitAmount;
-			return ++debitCount;
+			debitCount++;
 		}
 		else {
 			System.out.println("Please enter valid debit amount it should not excess current balance.");
-			return debitCount;
 		}
 	}
 	
-	int printBalance(String userName) {
+	//Print updated bank balance of the user
+	void printBalance(String userName) {
 		allUsersPrintBalance++;
 		System.out.println("\nCurrent Balance of " + userName + " is: " + currentBalance);
-		return ++printBalanceCount;
+		printBalanceCount++;
 	}
 	
-	void individualTransactionSummary(String userName,int userCreditCount,int userDebitCount,int userprintBalCount) {
-		System.out.println("\n" + userName +"'s Transaction Summary: Credit - " + userCreditCount + " times" + "  Debit - " + userDebitCount + " times " + "  Print Balance - " + userprintBalCount + " time \n");
+	//Print transaction summary of particular user
+	void individualTransactionSummary(String userName) {
+		System.out.println("\n" + userName +"'s Transaction Summary: Credit - " + creditCount + " times" + "  Debit - " + debitCount + " times " + "  Print Balance - " + printBalanceCount + " time \n");
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	}
 	
+	//Print transaction Summary of all the users
 	void allTransactionSummary() {
 		System.out.println("All Transaction Summary  : Credit - " + allUsersCreditCnt + " times" + "  Debit - " + allUsersDebitCnt + " times " + "  Print Balance - " + allUsersPrintBalance + " time ");		
 	}
@@ -57,29 +60,30 @@ public class BankingSystem {
 		BankingSystem bankingSystem2 = new BankingSystem();
 		
 		System.out.println("Aayushi's Banking transactions are as below: \n");
-		//User1 banking transactions
-		int userCreditCount = bankingSystem1.amountCreditedByUser(100000);
-		int userDebitCount = bankingSystem1.amountDebitedByUser(5000);
-		userCreditCount = bankingSystem1.amountCreditedByUser(1000);
-		int userPrintBalCount = bankingSystem1.printBalance("Aayushi");	
+		//User1 banking credit, debit and print bank balance transactions
+		bankingSystem1.amountCreditedByUser(100000);
+		bankingSystem1.amountDebitedByUser(5000);
+		bankingSystem1.amountCreditedByUser(1000);
+		bankingSystem1.printBalance("Aayushi");	
 		
 		
-		//print User1 Details
-		bankingSystem1.individualTransactionSummary("Aayushi",userCreditCount,userDebitCount,userPrintBalCount);
+		//print User1 Details of credit, debit and print bank balance transactions
+		bankingSystem1.individualTransactionSummary("Aayushi");
 		
 		System.out.println("Shivam's Banking transactions are as below: \n");
-		userCreditCount = bankingSystem2.amountCreditedByUser(10000);
-		userDebitCount = bankingSystem2.amountDebitedByUser(5000);
-		userCreditCount = bankingSystem2.amountCreditedByUser(9000);
-		userDebitCount = bankingSystem2.amountDebitedByUser(5000);
-		userCreditCount = bankingSystem2.amountCreditedByUser(9000);
-		userCreditCount = bankingSystem2.amountCreditedByUser(200);
-		userCreditCount = bankingSystem2.amountCreditedByUser(500);
+		//User2 banking credit, debit and print bank balance transactions
+		bankingSystem2.amountCreditedByUser(10000);
+		bankingSystem2.amountDebitedByUser(5000);
+		bankingSystem2.amountCreditedByUser(9000);
+		bankingSystem2.amountDebitedByUser(5000);
+		bankingSystem2.amountCreditedByUser(9000);
+		bankingSystem2.amountCreditedByUser(200);
+		bankingSystem2.amountCreditedByUser(500);
 		
-		//Print User2 Details
-		bankingSystem2.individualTransactionSummary("Shivam",userCreditCount,userDebitCount,bankingSystem2.user2printBalCount);
+		//Print User2 Details of credit, debit and print bank balance transactions
+		bankingSystem2.individualTransactionSummary("Shivam");
 		
-		//Print all users Transaction Summary
+		//Print all users Transaction Summary of credit, debit and print bank balance transactions
 		bankingSystem2.allTransactionSummary();
 		
 	}	
