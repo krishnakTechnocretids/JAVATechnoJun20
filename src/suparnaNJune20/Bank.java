7513cf7971c7c27a26dfe9545b5468f492722110
Suparna Nikam <nikam.suparna.s@gmail.com>
2020-06-17 18:03:00 +0530

    Suparna_Assignment_8_done _with commnetschange

--- src/suparnaNJune20/Bank.java
+++ src/suparnaNJune20/Bank.java
@@ -2,43 +2,53 @@ package suparnaNJune20;
 
 public class Bank {
 
-	static double atmBal=100000;
+	static double atmBal;
 	static int debitCnt, creditCnt, printBillCnt;
+	String custName;
+	static void setAtmData(double cnt) {
+		atmBal += cnt;
+	}
+
+	void  setName(String name) {
+		custName = name;
+	}
+
 	int debit,credit, printBill;
-	
-	void debitTransaction(int amt, String name) {
+
+	void debitTransaction(int amt) {
 		if (amt>atmBal)	{
-			System.out.println("No Sufficient BAl ");
+			System.out.println("No Sufficient BAl in ATM  " );
 			return;
 		}
 		else {
 			atmBal-=amt;
 			debit++;
 			debitCnt++;
-			System.out.println(amt+ " Rs debited sucesfully by " + name);
+			System.out.println(amt+ " Rs debited sucesfully  ");
 		}
 	}
-		void  creditTransaction (int amt, String name) {
-			 atmBal+=amt;
-				System.out.println(amt+ " Rs credited sucesfully by"+ name);
-				credit++;
-				creditCnt++;
+	void  creditTransaction (int amt) {
+		atmBal+=amt;
+		System.out.println(amt+ " Rs credited sucesfully by");
+		credit++;
+		creditCnt++;
 	}		
-		void  printBillTransaction() {
-			System.out.println(" Current Balance in Atm :"+atmBal);
-			printBill++;
-			printBillCnt++;
-		}
-	
-	void individualTransactionSumary(String custName) {
-		System.out.println("\n\n Total No of  trnasaction done by  "+custName +" are "+ (debit+credit+ printBill));
+	void  printBillTransaction() {
+
+		System.out.println(" Current Balance in Atm :"+ atmBal );
+		printBill++;
+		printBillCnt++;
+	}
+
+	void individualTransactionSumary() {
+		System.out.println("\n\n Total No of  transaction  done by  " + custName +" are "+ (debit+credit+ printBill));
 		System.out.println("Total No of Credit transaction " + credit);
 		System.out.println("Total No of Debit transaction " + debit);
 		System.out.println("Total No of PrintBill transaction "+ printBill);
-    }
-	
+	}
 
-	void allTransactionSumary() {
+
+	static void allTransactionSumary() {
 		System.out.println("\n\nTotal No of  trnasaction done by all custome are  "+ (debitCnt+creditCnt+ printBillCnt));
 		System.out.println("Total No of Credit transaction " + creditCnt);
 		System.out.println("Total No of Debit transaction " + debitCnt);
@@ -48,25 +58,31 @@ public class Bank {
 		// TODO Auto-generated method stub
 		Bank customer1 = new Bank();
 		Bank customer2 = new Bank();
-		System.out.println("Current ATM bal : "+customer1.atmBal );
-		for (int i = 40; i<45; i++)
-		{		if(i%3 == 1){
-					customer1.debitTransaction(3498*1+34,"Deepika");
-					customer2.printBillTransaction();
-				}
-				else  if (i%3 == 2)	{
-					customer1.creditTransaction(456/1+5-1, "Deepika");
-					customer2.debitTransaction(345656*2+698, "Ranveer");
-				}
-				else  if (i%3 == 0){
-					customer1.printBillTransaction();
-					customer2.creditTransaction(453423*9+5-1,"Ranveer");
-				}
-		}
-		customer1.individualTransactionSumary("Cutomer1");
-		customer2.individualTransactionSumary("Cutomer2");	
-		customer1.allTransactionSumary();
-		
+		setAtmData(100000);
+		customer1.setName("Deepika");
+		customer2.setName("Priyanka");
+		allTransactionSumary();
+		System.out.println("\n");
+		System.out.println("\n Current ATM bal : "+atmBal );
+		System.out.println("\n Transaction by " + customer1.custName);
+		customer1.debitTransaction(45634);
+		customer1.printBillTransaction();
+		customer1.creditTransaction(12345 );
+		System.out.println("\nTransaction by " + customer2.custName);	
+		customer2.debitTransaction(67890 );
+		customer2.printBillTransaction();
+		customer2.creditTransaction(1253123);
+		System.out.println("\nTransaction by " + customer1.custName);
+		customer1.printBillTransaction();
+		customer1.debitTransaction(45334);
+		System.out.println("\nTransaction by " + customer2.custName);	
+		customer2.printBillTransaction(); 
+		customer2.debitTransaction(4563445);
+		customer1.individualTransactionSumary();
+		customer2.individualTransactionSumary();
+
+		allTransactionSumary();
+
 	}
 
 }
