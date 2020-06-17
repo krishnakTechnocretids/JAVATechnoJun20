881c884f61429a5d538cecfd572ca83a36f4373d
Raj Adatiya <rajadatiya35@gmail.com>
2020-06-17 15:12:15 +0530

    Updated suggested changes in Raj_Assignment_8

--- src/rajAJun20/BankSystem.java
+++ src/rajAJun20/BankSystem.java
@@ -2,13 +2,13 @@ package rajAJun20;
 
 public class BankSystem {
 
-	int balance = 50000;
-	int creditCount = 0;
-	int debitCount = 0;
-	int printBalanceCount = 0;
-	static int creditCount1 = 0;
-	static int debitCount1 = 0;
-	static int printBalanceCount1 = 0;
+	int balance;
+	int creditCount;
+	int debitCount;
+	int printBalanceCount;
+	static int creditCount1;
+	static int debitCount1;
+	static int printBalanceCount1;
 
 	void debitAmount(int debit) {
 		if(balance >= debit) {
@@ -42,23 +42,23 @@ public class BankSystem {
 	public static void main(String[] args) {
 		BankSystem banksystem = new BankSystem();
 		BankSystem banksystem1 = new BankSystem();
-		banksystem.debitAmount(10000);
-		banksystem.creditAmount(30000);
-		banksystem.creditAmount(5000);		
+		banksystem.creditAmount(40000);
+		banksystem.creditAmount(10000);
+		banksystem.debitAmount(10000);		
 		banksystem.getCurrentBalance();
 		System.out.print("Transaction Summary for User 1 is : ");
 		banksystem.individualTransactionSummary();
 		System.out.println("                                           ");
-		banksystem1.debitAmount(5000);
-		banksystem1.debitAmount(3000);
 		banksystem1.creditAmount(10000);
 		banksystem1.creditAmount(2000);
 		banksystem1.creditAmount(4000);
 		banksystem1.creditAmount(100);
 		banksystem1.creditAmount(600);
+		banksystem1.debitAmount(5000);
+		banksystem1.debitAmount(3000);
 		System.out.print("Transaction Summary for User 2 is : ");
 		banksystem1.individualTransactionSummary();
-		System.out.println("----------------------------");
+		System.out.println("                                          ");
 		System.out.println("All Transaction summary: ");
 		banksystem.allTransactionSummary();		
 	}
