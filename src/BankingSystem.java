1dd501051addcc33e857f1e6a04110b1f92c7c2e
Piyush Pathak <piyush.p11@gmail.com>
2020-06-17 13:00:19 +0530

    Changes Incorporated

--- src/BankingSystem.java
+++ src/BankingSystem.java
@@ -1,28 +1,41 @@
 
 public class BankingSystem {
 
-	int creditCount, debitCount, printCount;
-	static int userCrdtCount;
-	static int userDebtCount;
-	static int userprintCount;
+	int creditCount, debitCount, printCount, balance;
+	static int userCrdtCount, userDebtCount, userprintCount, countUser;
+	
+	void setInitialBalance(int amount){
+		countUser++;
+		System.out.println("");
+		System.out.println(" Initial Balance of User "+countUser+" is: "+amount);
+		balance = amount;
+	}
 
 	void debitAccount(int debitAmt){
-        debitCount++;
-		userDebtCount++;
-	}
+		if(balance < debitAmt)
+			System.out.println(" Insufficient Balance ");
+		else{
+			balance -= debitAmt;
+			debitCount++;
+			userDebtCount++;
+			}
+        }
+	
 	void creditAccount(int creditAmt){
+		balance += creditAmt;
 		creditCount++;
 		userCrdtCount++;
 	}
+	
 	void printBalance(int crAmt,int debtAmt){
 	    System.out.println(""); 
-	    System.out.println(" Current Balance is : " + ((crAmt-debtAmt)));
+	    System.out.println(" Current balance of User "+countUser+" is: "+(balance));
 		printCount++;
 		userprintCount++;
 	}
-	void transactionSummary(String name){
+	void transactionSummary(){
 		System.out.println("");
-		System.out.println( name + "transaction Summary: Debit - " + debitCount + " times, Credit - " + creditCount + " times, PrintBalance " + printCount + " times");
+		System.out.println(" User " +countUser+" " + "transaction Summary: Debit - " + debitCount + " times, Credit - " + creditCount + " times, PrintBalance " + printCount + " times");
 	}
     void allTransactionSummary(){
     	System.out.println("");
@@ -32,14 +45,16 @@ public class BankingSystem {
 	public static void main(String[] args)
 	{
 		BankingSystem bankingsystem = new BankingSystem();
+		bankingsystem.setInitialBalance(5000);
 		bankingsystem.debitAccount(500);
 		bankingsystem.creditAccount(100);
 		bankingsystem.creditAccount(100);
 		bankingsystem.printBalance(600,300);
-		bankingsystem.transactionSummary(" User1 ");
+		bankingsystem.transactionSummary();
 		
 		
 		BankingSystem bankingsystem1 = new BankingSystem();
+		bankingsystem1.setInitialBalance(7000);
 		bankingsystem1.debitAccount(100);
 		bankingsystem1.debitAccount(200);
 		bankingsystem1.creditAccount(200);
@@ -47,7 +62,7 @@ public class BankingSystem {
 		bankingsystem1.creditAccount(300);
 		bankingsystem1.creditAccount(400);
 		bankingsystem1.creditAccount(500);
-		bankingsystem1.transactionSummary(" User2 ");
+		bankingsystem1.transactionSummary();
 		bankingsystem.allTransactionSummary();
 
 	}	
