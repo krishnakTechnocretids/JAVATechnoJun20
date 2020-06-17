8258c565564fce5544ec24b4c4b362d42d2edbea
Barkha Patle <barkhapatle01@gmail.com>
2020-06-17 11:51:43 +0530

    Barkha_Assignment_8_updated

--- src/barkhaPJun20/Bank.java
+++ src/barkhaPJun20/Bank.java
@@ -1,38 +1,28 @@
 package barkhaPJun20;
 
 public class Bank {
-	int debitCount=0;
-	int creditCount=0;
-	int balanceCount=0;
-	
-	static int overallDebitCount=0;
-	static int overallCreditCount=0;
-	static int overallBalanceCount=0;
-	
-	int debitAmnt;
-	int creditAmnt;
+	int debitCount;
+	int creditCount;
+	int balanceCount;
+	static int overallDebitCount;
+	static int overallCreditCount;
+	static int overallBalanceCount;
 	int currentBalance;
-	
-	
 	int user;
-	
 	void setInitialAmnt(int uID,int Amnt){
 		user=uID;
 		currentBalance=Amnt;
 	}
-	
 	void debitAmount( int dAmnt){
-		debitAmnt=dAmnt;
-		currentBalance=currentBalance-debitAmnt;
+		currentBalance=currentBalance-dAmnt;
 		debitCount++;
 		overallDebitCount++;
 	}	
 	void creditAmount( int cAmnt){
-		creditAmnt=cAmnt;
-		currentBalance=currentBalance+creditAmnt;
+		currentBalance=currentBalance+cAmnt;
 		creditCount++;
 		overallCreditCount++;
-	}
+	}		
 	void printBalance(){
 		System.out.println();
 		System.out.println("Final Amount is: "+currentBalance +" for customer "  +user);
@@ -51,7 +41,6 @@ public class Bank {
 		System.out.println ("Overall Print Balance done " +overallBalanceCount);
 	}
 	public static void main(String[] args){
-		
 		Bank bank1=new Bank();
 		bank1.setInitialAmnt(1,1000);
 		bank1.debitAmount(50);
@@ -59,8 +48,6 @@ public class Bank {
 		bank1.creditAmount(300);
 		bank1.printBalance();
 		bank1.individualTransactionSummary();
-		
-		
 		Bank bank2=new Bank();
 		bank2.setInitialAmnt(2,2000);
 		bank2.debitAmount(190);
@@ -72,13 +59,26 @@ public class Bank {
 		bank2.creditAmount(90);
 		bank2.printBalance();
 		bank2.individualTransactionSummary();
-		
-		
 		bank2.allTransactionSummary();
-		
 	}
+}				
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+	
 
-}
+
+	
+	
+	
+	
 
 		
 	
