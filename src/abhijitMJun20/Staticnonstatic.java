c083b0eb99b3667c4ff5ad7c0e4521231911a0fd
Abhijit Mali <abhijitmali94@gmail.com>
2020-06-18 18:45:20 +0530

    updated suggested changes.

--- src/abhijitMJun20/Staticnonstatic.java
+++ src/abhijitMJun20/Staticnonstatic.java
@@ -1,56 +1,52 @@
 package abhijitMJun20;
 
-public class Staticnonstatic { 
-	static double sumofvalues;
-	static double average;
+public class Staticnonstatic{ 
 	double sum( double parameter1, double parameter2,double parameter3){
-		 sumofvalues=parameter1+parameter2+parameter3;
+		double sumofvalues=parameter1+parameter2+parameter3;
 		return sumofvalues;
 	}
 	double average(double parameter4, double parameter5,double parameter6){  
-	       sum(parameter4,parameter5,parameter6);
-		   average=sumofvalues/3;
-		return average;
+		 double averageofsum=sum(parameter4,parameter5,parameter6)/3;
+		return averageofsum;
 	}
-	static boolean isAnswerAboveExpecation(double parameter7 ){
-	     if(parameter7>50){
-		 return true;
-		 }
-	     else { 
-			 return false;
-			 }
+	static boolean isAnswerAboveExpecation(double averageofsum ){
+	     if(averageofsum>50)
+		 {
+		 return true;}
+	 else { return false;}
 	}
-	static boolean isEligible(double parameter8){
-		if( parameter8%2==0 || parameter8>50){
+	static boolean isEligible(double averageofsum){
+		if( averageofsum%2==0 || averageofsum>50){
 			return true;
 		}else{
 			return false;
-		}		
+			}		
 	}
-	static String getYourGrade(double parameter9 ){
-	     if(parameter9>80){
-		 return "A Grade";
-		 }
-	 else {
-		 return "B Grade";
-		 }
+	static String getYourGrade(double averageofsum ){
+	     if(averageofsum>80)
+		 {
+		 return "A Grade";}
+	 else { return "B Grade";}
 	}
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
 		Staticnonstatic staticnonstatic= new Staticnonstatic();
-		staticnonstatic.average(10,20,50000);
-		if(isAnswerAboveExpecation(average)){
+		double averageofsum=staticnonstatic.average(10,20,50000);
+		if(isAnswerAboveExpecation(averageofsum))
+		{
 			System.out.println("Value is more then 50: True");
 		}
 		else{
 			System.out.println("Value is more then 50: False");
 		}
-		if(isEligible(average)){
+		if(isEligible(averageofsum))
+		
+			{
 			System.out.println("Eligibility is true");
 		}
 		else{
 			System.out.println("Eligibility is false");
 		}
-		System.out.println(getYourGrade(average));
+		
+		System.out.println(getYourGrade(averageofsum));
 	}
 }
