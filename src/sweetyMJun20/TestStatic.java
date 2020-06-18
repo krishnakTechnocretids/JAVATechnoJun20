ee374c6eb167aae3a6cbcd0e311c1654c5fd97e8
Sweety Modi <sweety.dhyey@gmail.com>
2020-06-18 22:08:34 +0530

    Sweety_Assignment_9_changes

--- src/sweetyMJun20/TestStatic.java
+++ src/sweetyMJun20/TestStatic.java
@@ -1,26 +1,29 @@
 package sweetyMJun20;
 
 public class TestStatic {
-	    static double ans;
+        static double ans;
 		static double avg;
+		
 		double sum(double n1,double n2, double n3){
-			ans=n1+n2+n3;
-			return ans;
+			//ans=n1+n2+n3;
+			return n1+n2+n3;
 		}
 		double average(double n1,double n2,double n3){
-			avg=sum(n1, n2, n3)/3;
-			return avg;
+			//avg=sum(n1, n2, n3)/3;
+			return sum(n1,n2,n3)/3;
 		}
 		static boolean isAnswerAboveExpecation( double isans1){
 			if (isans1>=50){
+				System.out.println("Answer is above expectation: " + isans1);
 				return true;
 			}
-			else{
+			else{ 
+			    System.out.println("Answer is not above expectation: " + isans1);
 				return false;
 			}
 		}
 		static boolean isEligible(double isans){
-			if (isans>=50 || isans/2==0)
+			if (isans>=50 || isans%2==0)
 				return true ;
 			else
 				return false;
@@ -34,14 +37,14 @@ public class TestStatic {
 		
 		public static void main(String[] args){
 			TestStatic teststatic1=new TestStatic();
-			double isans1=teststatic1.average(30.00,20.00,50.00);
-			System.out.println(isAnswerAboveExpecation(isans1));
-			if (isEligible(isans1)==true)
+			double isans1=teststatic1.average(150.00,150.00,300.00);
+			//System.out.println(TestStatic.isAnswerAboveExpecation(isans1));
+			TestStatic.isAnswerAboveExpecation(isans1);
+			if (TestStatic.isEligible(isans1)==true)
 				System.out.println(" is eligible");
 			else
 				System.out.println(" is not eligible");
-			System.out.println(getYourGrade(isans1));
-			}
+			System.out.println(TestStatic.getYourGrade(isans1));
+			
+		}
 	}
-
-
