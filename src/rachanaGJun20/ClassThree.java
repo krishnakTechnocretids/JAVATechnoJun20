ece141d51532a4cc6e71fd56493aab1f344f3e60
Rachana Ghayal <rach.g14@gmail.com>
2020-06-18 21:13:52 +0530

    Rachana_Assignment_10_Done

--- src/rachanaGJun20/ClassThree.java
+++ src/rachanaGJun20/ClassThree.java
@@ -4,12 +4,12 @@ public class ClassThree {
 
 	static void methodFive() {
 		System.out.println("\nHey I am in methodFive of ClassThree !");
-		System.out.println("Method Type : Static  Called from : Non-Static(methodSix|ClassThree)\nTo call me : Object & Class name not required,Call Directly");
+		System.out.println("Method Type : Static  Called from : Non-Static(methodSix|ClassThree)\nCalling Type: Static - Non-Static\nTo call me : Object & Class name not required,Call Directly");
 	}
 	
 	 void methodSix() {
 		 System.out.println("\nHey I am in methodSix of ClassThree !");
-		 System.out.println("Method Type : Non-Static  Called from : Non-Static(methodThree|ClassTwo) \nTo call me : Object required");
+		 System.out.println("Method Type : Non-Static  Called from : Non-Static(methodThree|ClassTwo)\nCalling Type: Non-Static - Non-Static \nTo call me : Object required");
 		 methodFive();
 	}
 }
--- src/rachanaGJun20/ClassTwo.java
+++ src/rachanaGJun20/ClassTwo.java
@@ -4,7 +4,7 @@ public class ClassTwo {
 
 		void methodThree() {
 			System.out.println("\nHey I am in methodThree of ClassTwo !");
-			System.out.println("Method Type : Non-Static  Called from : Static(mathodFour|ClassTwo) \nTo call me : Object required");
+			System.out.println("Method Type : Non-Static  Called from : Static(mathodFour|ClassTwo)\nCalling Type: Non-Static - Static \nTo call me : Object required");
 			ClassThree classthree = new ClassThree();
 			classthree.methodSix();
 			
@@ -12,7 +12,7 @@ public class ClassTwo {
 		
 		static void methodFour() {
 			System.out.println("\nHey I am in methodFour of ClassTwo !");
-			System.out.println("Method Type : Static  Called from : Static(methodTwo|ClassOne) \nTo call me : Class Name required");
+			System.out.println("Method Type : Static  Called from : Static(methodTwo|ClassOne)\nCalling Type: Static - Staic \nTo call me : Class Name required");
 			ClassTwo classtwo = new ClassTwo();
 			classtwo.methodThree();
 		}
