1cb7394ae419938490f683ea8807ac47346a80e0
Piyush Pathak <piyush.p11@gmail.com>
2020-06-22 00:06:59 +0530

    Assignment Done

---
+++ src/NameLength.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class NameLength {
+	
+	void printLength(String[] input) {
+		String max = input[0];
+		for(int index=0; index<input.length;index++) {
+			if(input[index].length() > max.length())
+				max = input[index];
+		}
+		System.out.println(" Maximum length of String is:" + max.length());
+	}
+
+	public static void main(String[] args) {
+		NameLength namelength = new NameLength();
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(" How many Elements you want to enter: ");
+		int size = scanner.nextInt();
+		String[] name = new String[size];
+		
+		for(int index=0; index<name.length; index++) {
+			System.out.println(" Enter Name" + (index+1) + ":");
+			name[index] = scanner.next();
+		}
+		
+		namelength.printLength(name);
+		scanner.close();
+	}
+	
+	
+}
