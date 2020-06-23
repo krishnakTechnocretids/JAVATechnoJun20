739acc181a1a327ca03f85531bf84db3bbf424cf
Shruti_Dubey <shrutidubey69@gmail.com>
2020-06-23 17:32:40 +0530

    Shruti_Assignment15_1hour

---
+++ src/shrutiDJun20/SumOfEven.java
@@ -0,0 +1,48 @@
+/*
+ You need to store user input in array and do sum of even numbers only from created 
+     array. [ideal Expected Time : 10 mins, Max Time : 15 mins]
+
+     Example :
+     Input : {10,5,7,3,1,5,9,30}
+     Output: 40
+
+ */
+package shrutiDJun20;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumOfEven {
+
+	void sumEven(int[] num){
+		//int count = 0;
+		int sum=0;
+		for(int index = 0; index<num.length ; index++){
+			if(num[index]%2==0)
+				sum = sum+num[index];
+		}
+		//System.out.println(int[] num);
+		System.out.println("Sum of even inputs:" +sum);
+		
+		
+	}
+	public static void main(String[] args) {
+		SumOfEven sumOfEven = new SumOfEven();
+		Scanner scanner = new Scanner(System.in);
+		//int size= scanner.nextInt();
+		System.out.println("How many inputs you want to provide");
+		int size= scanner.nextInt();
+		int[] number = new int[size];
+		for(int index=0;index<size;index++){
+		System.out.println("Input"+(index+1));
+		number[index]=scanner.nextInt();
+		}
+		//System.out.println( Arrays.toString( arr ));
+		System.out.println(Arrays.toString(number));
+		sumOfEven.sumEven(number);
+		
+		
+		scanner.close();
+		
+	}
+}
