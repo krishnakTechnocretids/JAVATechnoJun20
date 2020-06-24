ce1b893ece2f076467e24a2b8fcb80fc1f29d43b
VireshJ <jangamviresh@yahoo.com>
2020-06-24 20:05:28 +0530

    Viresh_Assignment_16_Done_60Min

---
+++ src/vireshJJun20/Assignment16/CharOps.java
@@ -0,0 +1,67 @@
+package vireshJJun20.Assignment16;
+
+import java.util.Scanner;
+
+public class CharOps {
+
+	void oneCharFreq(String Name, String chr){
+		int cnt=0;
+		for(int i= 0; i<Name.length(); i++){
+			String temp= String.valueOf(Name.charAt(i));
+			if (temp.equals(chr)){
+				cnt++;
+			}
+		}
+		if (cnt!=0)
+			System.out.println(" - Given Character " + chr + " is present " + cnt +" times in a given name: " + Name);
+		else
+			System.out.println(" - Given Character " + chr + " is not present in a given name: " + Name);
+	}
+
+	void AllCharFreq(String Name){ //aabc
+		
+		for (int i=0; i<Name.length(); i++){
+			int cnt=0;
+			for (int j=0; j<Name.length(); j++){
+				if (Name.charAt(i)==Name.charAt(j)){
+					cnt++;
+				}		
+			}
+			System.out.println(" - " + Name.charAt(i) + " > " + cnt);
+		}
+	}
+	
+	void AllCharFreqArray(String[] arr){
+		
+		for (int i= 0; i<arr.length; i++){
+			AllCharFreq(arr[i]);
+			System.out.println("=======================");
+		}
+	}
+	
+	public static void main(String[] args){
+		CharOps charOps= new CharOps();
+		Scanner scanner= new Scanner(System.in);
+		String[] arr= {"raj", "aarya", "aavruti", "shruti"};
+		System.out.println("**Below logic is to search the asked character frequency in the asked name.");
+		System.out.print(" - Enter a name: ");
+		String name= scanner.next();
+		System.out.print(" - Enter a Character: ");
+		String chr= scanner.next();
+		charOps.oneCharFreq(name, chr);
+		System.out.print("**Do you want to check the logic: to search the frequency of each character in a predefined name? (y/n)");
+		String tmp1= scanner.next();
+		if (tmp1.equals("y")){
+			System.out.println("The name is: aakanksha");
+			charOps.AllCharFreq("aakanksha");
+		}
+		System.out.print("**Do you want to check the logic: to search the frequency of each character in each name of a pre-defined aray? (y/n)");
+		String tmp2= scanner.next(); 
+			if (tmp2.equals("y")){
+				System.out.println(" Array: raj, aarya, aavruti, shruti");
+				charOps.AllCharFreqArray(arr);
+			}	
+		scanner.close();		
+	}
+	
+}
