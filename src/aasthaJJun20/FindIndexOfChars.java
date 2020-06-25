2d596085a880f98e3a20e411e68afb400e431ede
Aastha Jaiswal <jaiswal.aastha1691@gmail.com>
2020-06-25 20:57:09 +0530

    Aastha_Assignment_17_Done_60mins

--- src/aasthaJJun20/FindIndexOfChars.java
+++ src/aasthaJJun20/FindIndexOfChars.java
@@ -3,16 +3,11 @@ package aasthaJJun20;
 public class FindIndexOfChars {
 	void countChars(String input){
 		for(int index = 0; index < input.length(); index++){
-			int count = 0;
-			//char ch = input.charAt(index);
 			if(index == input.indexOf(input.charAt(index))){
-				for(int innerIndex = 0; innerIndex < input.length(); innerIndex++){
-					if(input.charAt(index) == input.charAt(innerIndex))
-						count++;
+				if(input.indexOf(input.charAt(index)) != input.lastIndexOf(input.charAt(index))){
+					System.out.println(input.charAt(index) + " -> first index = " + input.indexOf(input.charAt(index))+" Last index = "+input.lastIndexOf(input.charAt(index)));
 				}
 			}
-			if(count>1)
-				System.out.println(input.charAt(index) + " -> first index = " + input.indexOf(input.charAt(index))+" Last index = "+input.lastIndexOf(input.charAt(index)));
 		}
 	}
 	
