5074ac46bae181706f8194f2c91d860ff988177a
Shruti_Dubey <shrutidubey69@gmail.com>
2020-06-25 23:30:07 +0530

    added print statement for all letter

--- src/shrutiDJun20/IndexOfEx2.java
+++ src/shrutiDJun20/IndexOfEx2.java
@@ -11,13 +11,16 @@ public class IndexOfEx2 {
 		int upperCount=0;
 		int lowerCount=0;
 		int specialCount=0;
+		int totalCount=0;
 		
 		//1rRpd3F9#K(E
 		for(int i=0;i<input.length();i++){
 			char ch = input.charAt(i);
 			if(Character.isDigit(ch))
 					digitCount++;	
-			
+			else if(Character.isLetter(ch))
+				totalCount++;
+	
 			else if(Character.isLowerCase(ch))
 				lowerCount++;
 			
@@ -31,7 +34,8 @@ public class IndexOfEx2 {
 		System.out.println("Count of digit in the given string is "+ digitCount);
 		System.out.println("Count of uppercase in the given string is "+ upperCount);
 		System.out.println("Count of lowercase in the given string is "+ lowerCount);
-		System.out.println("Count of special Charecter in the given string is "+ specialCount);
+		System.out.println("Count of special Character in the given string is "+ specialCount);
+		System.out.println("Count of all Character in the given string is "+ totalCount);
 
 	}
 	
