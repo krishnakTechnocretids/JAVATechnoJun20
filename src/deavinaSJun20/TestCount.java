ebf332cc6ec85d168096b56ce5b8cc1829e831e7
Deavina Sharma <deavina.sharma@gmail.com>
2020-06-25 22:52:59 +0530

    Review Comments Incorporated

--- src/deavinaSJun20/TestCount.java
+++ src/deavinaSJun20/TestCount.java
@@ -13,28 +13,25 @@ Total Special char: 2
 public class TestCount {
 	public static void main(String[] args) {
 		String name = "1rRpd3F9#K(E";
-		System.out.println("Entered String : "+name);
+		System.out.println("Entered String : " + name);
 		new TestCount().countDigitLetter(name);
 	}
 
 	void countDigitLetter(String name) {
-		int countDigit = 0, countUppercase = 0, countLowercase = 0, countSpecialchar = 0, countLetter = 0;
+		int countDigit = 0, countUppercase = 0, countLowercase = 0, countSpecialchar = 0;
 		for (int i = 0; i < name.length(); i++) {
 			if (Character.isDigit(name.charAt(i))) {
 				countDigit++;
-			} else if (Character.isLetter(name.charAt(i))) {
-				countLetter++;
-				if (Character.isLowerCase(name.charAt(i))) {
-					countLowercase++;
-				} else if (Character.isUpperCase(name.charAt(i))) {
-					countUppercase++;
-				}
+			} else if (Character.isLowerCase(name.charAt(i))) {
+				countLowercase++;
+			} else if (Character.isUpperCase(name.charAt(i))) {
+				countUppercase++;
 			} else {
 				countSpecialchar++;
 			}
 		}
 		System.out.println("Total Digits in String : " + countDigit);
-		System.out.println("Total Letters in String : " + countLetter);
+		System.out.println("Total Letters in String : " + (countUppercase + countLowercase));
 		System.out.println("Total Uppercase in String : " + countUppercase);
 		System.out.println("Total Lowercase in String : " + countLowercase);
 		System.out.println("Total Special characaters in String : " + countSpecialchar);
