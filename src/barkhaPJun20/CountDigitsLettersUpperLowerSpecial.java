7d888189bb1c7cd6379d416815daf3a6a8aa6f01
Barkha Patle <barkhapatle01@gmail.com>
2020-06-25 23:42:52 +0530

    Barkha_Assignment_17_changes_Done

--- src/barkhaPJun20/CountDigitsLettersUpperLowerSpecial.java
+++ src/barkhaPJun20/CountDigitsLettersUpperLowerSpecial.java
@@ -13,22 +13,25 @@ public class CountDigitsLettersUpperLowerSpecial {
 			
 			if (Character.isDigit(ch)) {
 				countDigit++;
-			} else if (Character.isLetter(ch)) {
-				countLetters++;
-				if (Character.isUpperCase(ch)) {
+			}
+				else if (Character.isUpperCase(ch)) {
 					countUpperCase++;
-				}else
-						countLowerCase++;
 				}
-			else
-				countSpecialChar++;
+				else if(Character.isLowerCase(ch)) {
+					countLowerCase++;
+				}
+				else
+					countSpecialChar++;
+			
 			}
 		System.out.println("The DIGITS present in "+name +" are: "+countDigit);
-		System.out.println("The LETTERS present in "+name +" are: "+countLetters);	
+		//System.out.println("The LETTERS present in "+name +" are: "+countLetters);	
+		System.out.println("The LETTERS present in "+name +" are: "+(countUpperCase+countLowerCase));
 		System.out.println("The UPPER CASE LETTERS present in "+name +" are: "+countUpperCase);	
 		System.out.println("The LOWER CASE LETTERS present in "+name +" are: "+countLowerCase);	
 		System.out.println("The SPECIAL CHARACTERS present in "+name +" are: "+countSpecialChar);	
-}		
+
+	}
 	public static void main(String[] args) {
 	CountDigitsLettersUpperLowerSpecial count=new  CountDigitsLettersUpperLowerSpecial();
 	String name="1rRpd3F9#K(E";	
