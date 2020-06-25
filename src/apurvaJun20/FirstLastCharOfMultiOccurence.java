dabe96bab85189cb2c75d4ddb70dc681772d5691
Apurva <apurvagupta1410@gmail.com>
2020-06-26 01:28:47 +0530

    Apurva_Assignment_17_90minandmore

---
+++ src/apurvaJun20/FirstLastCharOfMultiOccurence.java
@@ -0,0 +1,33 @@
+package apurvaJun20;
+
+public class FirstLastCharOfMultiOccurence {
+
+		void displayCount(String str) {
+			int upper = 0, lower = 0, digit = 0, special = 0; 
+
+			for(int index=0;index<str.length();index++) {
+				char ch=str.charAt(index);
+				if(Character.isDigit(ch))
+					digit++;
+				else if(Character.isUpperCase(ch))
+					upper++;
+				else if(Character.isLowerCase(ch))
+					lower++;
+				else
+					special++;
+			}
+			System.out.println("Total Digit:--------> "+digit);
+			System.out.println("Total UpperCase:----> "+upper);
+			System.out.println("Total LowerCase:----> "+lower);
+			System.out.println("Total SpecialChar:--> "+special); 
+			System.out.println("Total Digit:--------> "+digit + "\nTotal Letters:------> "+(upper+lower)+ "\nTotal UpperCase:----> "+upper+"\nTotal LowerCase:----> "+lower+"\nTotal SpecialChar:--> "+special);
+		}
+
+		public static void main(String[] args) {
+			FirstLastCharOfMultiOccurence firstLastCharOfMultiOccurence= new FirstLastCharOfMultiOccurence();
+
+			String str="1rRpd3F9#K(E";
+			firstLastCharOfMultiOccurence.displayCount(str);
+		}
+
+}
