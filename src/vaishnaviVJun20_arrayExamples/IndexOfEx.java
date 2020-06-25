fae637811cab997db98d31b1e203efded8a6aef3
Vaishnavi Vaidya <vaishnavivaidya06@gmail.com>
2020-06-25 23:10:57 +0530

    Vaishnavi_Assignment_17_Changes_Done

--- src/vaishnaviVJun20_arrayExamples/IndexOfEx.java
+++ src/vaishnaviVJun20_arrayExamples/IndexOfEx.java
@@ -3,17 +3,11 @@ package vaishnaviVJun20_arrayExamples;
 public class IndexOfEx {
 	
 	void printFirstAndLastIndex(String s) {
+		
 		for(int index=0;index<s.length();index++) {
 			char ch = s.charAt(index);
-			int count=0;
-			if(index == s.indexOf(ch)) {
-				for(int i=0;i<s.length();i++) {
-					if(ch== s.charAt(i))
-						count++;				
-				}
-				if(count>1)
-					System.out.println(ch +"--> first index= " +s.indexOf(ch) +" and Last index=" +s.lastIndexOf(ch) );
-			}
+			if(index == s.indexOf(ch) && s.indexOf(ch) != s.lastIndexOf(ch))
+				System.out.println(ch +"--> first index= " +s.indexOf(ch) +" and Last index=" +s.lastIndexOf(ch) );
 		}
 	}
 
--- src/vaishnaviVJun20_arrayExamples/StringMethods.java
+++ src/vaishnaviVJun20_arrayExamples/StringMethods.java
@@ -6,22 +6,19 @@ public class StringMethods {
 		int digitCount=0;
 		int lowerCaseCount=0;
 		int upperCaseCount=0;
-		int letterCount=0;
 		int specialCharCount=0;
 		for(int index=0;index<s.length();index++) {
 			if(Character.isDigit(s.charAt(index)))
 				digitCount++;
-			if(Character.isLetter(s.charAt(index)))
-				letterCount++;
-			if(Character.isUpperCase(s.charAt(index)))
+			else if(Character.isUpperCase(s.charAt(index)))
 				upperCaseCount++;
-			if(Character.isLowerCase(s.charAt(index)))
+			else if(Character.isLowerCase(s.charAt(index)))
 				lowerCaseCount++;
 			else
 				specialCharCount++;
 		}
 		System.out.println("Total Digit: "+digitCount);
-		System.out.println("Total Letter: "+letterCount);
+		System.out.println("Total Letter: "+(lowerCaseCount+upperCaseCount));
 		System.out.println("Total LowerCase: "+lowerCaseCount);
 		System.out.println("Total UpperCase: "+upperCaseCount);
 		System.out.println("Total SpecialCharacter: "+specialCharCount);
