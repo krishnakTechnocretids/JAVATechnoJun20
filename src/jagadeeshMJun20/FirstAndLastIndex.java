debae2a862baeb064a8f482e50618cff33f29e0b
Jagadeesh Mangipudi <jagadeesh.mangipudi412@gmail.com>
2020-06-26 23:58:13 +0530

    Jagadeesh_Assignment_17_60min

--- src/jagadeeshMJun20/FirstAndLastIndex.java
+++ src/jagadeeshMJun20/FirstAndLastIndex.java
@@ -4,23 +4,18 @@ public class FirstAndLastIndex {
 		void printCharFreq(String name) {
 			for(int index=0; index<name.length(); index++) {
 				char ch = name.charAt(index);//t
-				int count = 0;
+				//int count = 0;
 				if (index == name.indexOf(ch)) {
-				     for(int i=0; i<name.length(); i++) {
-					 if (ch == name.charAt(i)) 
-						count++;
-						 if (count>1) {
-								int num = name.indexOf(ch);
-								if (ch == name.charAt(i)) 
-							 System.out.println(ch+ "  -->  first index = "+num+" and Last index  = "+ i); 
-						 }
-				     }
-		          }
+				if(name.indexOf(ch) != name.lastIndexOf(ch)) 
+					System.out.println(ch + " --> "+" First Index " + name.indexOf(ch) + " Last Index " + name.lastIndexOf(ch) );
 			}
-	} 
+			}
+			
+		}
 	public static void main(String[] args) {
 		FirstAndLastIndex firstAndLastIndex = new FirstAndLastIndex();
-		String name = "technocredits";
+		String name = "teechnocredits";
 		firstAndLastIndex.printCharFreq(name);
 	}
 }
+	
