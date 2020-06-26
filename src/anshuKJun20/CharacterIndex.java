da2411133ff5b75330c32a2b2ee84a5dce196066
Anshu Kumari <anshu.v.kumari02@gmail.com>
2020-06-26 20:34:22 +0530

    Anshu-Assignment-17 - 20min-ChangesDone

--- src/anshuKJun20/CharacterIndex.java
+++ src/anshuKJun20/CharacterIndex.java
@@ -3,27 +3,19 @@ package anshuKJun20;
 public class CharacterIndex {
 	
  void findIndex(String name) {
-	 for(int index = 0 ;index < name.length();index++) {
-		 int count=0;
+	 for(int index = 0 ;index < name.length();index++){
 		 char ch = name.charAt(index);
-		 if(name.indexOf(ch)== index) {
-		    for(int i =0;i<name.length();i++) {
-		        if(name.charAt(i)== ch) {
-			      count++;
-			        if(count>1) {
-				       System.out.println("Index of "+ch+name.indexOf(ch)+"last index of"+ch+name.lastIndexOf(ch));
-				        break;
-			        }
-		          }
-		       }
-		     }
+		   if(name.indexOf(ch)== index) {
+		      if(name.indexOf(ch)!=name.lastIndexOf(ch)) {
+		    	  System.out.println(ch+"--> first index : "+name.indexOf(ch)+" ,Last index : "+name.lastIndexOf(ch)+"\n");
+		      }
+            }
 	      }
-       }
+        }
 		 
-		 
-    public static void main(String[] args) {
+	 public static void main(String[] args) {
 	 CharacterIndex charcterindex = new CharacterIndex();
 	 String name = "technocredits";
-	 charcterindex.findIndex(name);
-}
-}
+	  charcterindex.findIndex(name);
+	 }
+   }
--- src/anshuKJun20/CountMultipleCharacter.java
+++ src/anshuKJun20/CountMultipleCharacter.java
@@ -1,30 +1,26 @@
 package anshuKJun20;
 
 public class CountMultipleCharacter {
-   void findcharcter(char[] ch) {
-	   int countdigit =0,countSpecialChar=0, countUpperCase=0,countLowerCase =0,countletters =0;
-	   for(int index = 0; index<ch.length;index++) {
-		   if(Character.isDigit(ch[index])==true) {
+   void findcharcter(String name) {
+	   int countdigit =0,countSpecialChar=0, countUpperCase=0,countLowerCase =0;
+	   for(int index = 0; index<name.length();index++) {
+		   char ch = name.charAt(index);
+		   if(Character.isDigit(ch)==true) {
 			   countdigit++;
-		   } else if(Character.isLetter(ch[index])){
-			   countletters++;
-			   if(Character.isLowerCase(ch[index])==true) {
+		       }else if(Character.isLowerCase(ch)==true) {
 				   countLowerCase++;
-				} if(Character.isUpperCase(ch[index])==true) {
+		       }else if(Character.isUpperCase(ch)==true) {
 					countUpperCase++;
-			   }
-			   
-		   }else {
+				}else {
 			   countSpecialChar++;
-		   }
-	   }
-	   
-	   System.out.println("Total Digit is:"+countdigit+"'\n' Total Letters:"+countletters+"'\n' Total Upper Case:"+countUpperCase+"'\n' Total Lower Case:"+countLowerCase+"'\n' Total Special character:"+countSpecialChar);
+			   }
+	         }
+	     System.out.println("Total Digit is:"+countdigit+"'\n' Total Upper Case:"+countUpperCase+"'\n' Total Lower Case:"+countLowerCase+"'\n' Total Special character:"+countSpecialChar);
    }
    public static void main(String[] args) {
 	   CountMultipleCharacter countmultiplecharcter = new CountMultipleCharacter();
-	   char[] ch = {'1','2','k','r','#','E','f','(','F','3','&'};
-	      countmultiplecharcter.findcharcter(ch);
+	   String letter = ("1rRpd3F9#K(E");
+	   countmultiplecharcter.findcharcter(letter);
 }
 }
 
