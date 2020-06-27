c69383068ef1f37b6e983bef02cbf697e58312b6
Mahesh Kumavat <mkumavat31@gmail.com>
2020-06-27 12:21:09 +0530

    Mahesh_Coding_Exam2_10Mins_Done

--- src/maheshKJun20/Test.java
+++ src/maheshKJun20/Test.java
@@ -1,16 +1,26 @@
 package maheshKJun20;
 
 public class Test {
-	
-	void display() {
-		
-		System.out.println("First sample git commit");
+
+	void frequencyOfNumber(int[] array, int number) {
+		int count = 0;
+
+		for (int index = 0; index < array.length; index++) {
+			if (array[index] == number)
+				count++;
+		}
+		if (count == number)
+			System.out.println("Given Number " + number + " is not present in predefined array ");
+		else
+			System.out.println("Given Number " + number + " is present in predefined array with frequency " + count);
 	}
-	
+
 	public static void main(String[] args) {
 		Test test = new Test();
-		test.display();
+		int[] array1 = { 10, 3, 9, -99, 81, 3, -11 };
+		test.frequencyOfNumber(array1, 30);
+		test.frequencyOfNumber(array1, -99);
+		test.frequencyOfNumber(array1, 11);
 	}
 
 }
-
