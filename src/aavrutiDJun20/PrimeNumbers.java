b48e3d8be63478450d9dae18f7f631a75f3aefc4
Aavruti Dalmia <aavruti14@gmail.com>
2020-06-27 20:17:18 +0530

    Aavruti_Assignment_19_OptimizedPrimeNo

--- src/aavrutiDJun20/PrimeNumbers.java
+++ src/aavrutiDJun20/PrimeNumbers.java
@@ -15,15 +15,17 @@ public class PrimeNumbers {
 			System.out.println(number + " is a Prime Number");		
 	}
 	
-	//Calculate Sum, Count and Average of Prime Numbers
+	//Calculate Sum, Count and Average of Prime Numbers between 2 - 100
 	void primeNumber2To100() {
 		int primeCnt = 0;
 		int sumOfPrime = 0;
 		for(int index=2;index<101;index++) {
 			boolean flag = true;
-			for(int index2=(index+1)/2;index2>=2;index2--) {
-				if(index%index2==0)
+			for(int index2=(index)/2;index2>=2;index2--) {
+				if(index%index2==0){
 					flag = false;
+					break;
+				}
 			}
 			if(flag==true) {
 				sumOfPrime += index;
