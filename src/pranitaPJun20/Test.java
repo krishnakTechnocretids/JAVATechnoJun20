6320b1e32511c0b49965cb238c788e9afeaad800
Pranita Puranik <pranita.mini@gmail.com>
2020-06-27 12:17:36 +0530

    Pranita_Coding_Exam_2_Done_10mins

--- src/pranitaPJun20/Test.java
+++ src/pranitaPJun20/Test.java
@@ -1,5 +1,35 @@
+/*
+ * String input = "1Hh9PR34QP";
+Hint : 1+9+3+4 [all digits]
+output : 17
+ */
 package pranitaPJun20;
 
 public class Test {
+	
+	public static void main(String[] args) {
+		Test test=new  Test();
+		String input ="1Hh9PR34QP";
+		test.findSumDigitCharString(input);
+	}
+		
+	void findSumDigitCharString(String input) {
+		int countDigit=0;
+		int sum=0;
+		String temp ="";
 
+		for(int index=0; index<input.length(); index++) {
+			char ch = input.charAt(index);
+				if(Character.isDigit(ch)) {
+					countDigit++;
+					temp= String.valueOf(ch);
+					int number1=Integer.parseInt(temp);
+					sum=sum+number1;
+				}
+		}
+			System.out.println("Sum of the digits in input string:"+sum);
+			System.out.println("Count of the digits in input string:"+countDigit);
+			
+
+	}
 }
