5da96678a8f5d61484c6c2aa81597c5678de4749
Deavina Sharma <deavina.sharma@gmail.com>
2020-06-27 12:45:19 +0530

    Updated File checked in as per review comments

--- src/deavinaSJun20/TestIntSumClass.java
+++ src/deavinaSJun20/TestIntSumClass.java
@@ -1,4 +1,5 @@
 package deavinaSJun20;
+
 /*String input = "1Hh9PR34QP";
 Hint : 1+9+3+4 [all digits]
 output : 178*/
@@ -6,20 +7,19 @@ public class TestIntSumClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String name="1Hh9PR34QP";
-
-new TestIntSumClass().intSumClass(name);
+		// String name = "1HhPR34QP";
+		String name = "123ch34";
+		new TestIntSumClass().calculateSumOfDigitsInString(name);
 	}
-  void intSumClass(String name) {
-	  int sum=0;
-	  for(int i=0;i<name.length();i++) {
-		  if(Character.isDigit(name.charAt(i)))
-		  {String ch=String.valueOf(name.charAt(i));
-			sum=sum+Integer.parseInt(ch)  ;
-			  
-			  
-		  }
-	  }
-	  System.out.println(sum);
-  }
+
+	void calculateSumOfDigitsInString(String name) {
+		int sum = 0;
+		for (int i = 0; i < name.length(); i++) {
+			if (Character.isDigit(name.charAt(i))) {
+				String ch = String.valueOf(name.charAt(i));
+				sum = sum + Integer.parseInt(ch);
+			}
+		}
+		System.out.println("Sum of all digits: " + sum);
+	}
 }
