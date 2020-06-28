f7bd1ff854cad5136d0b5ae187dfd54fcb7e89e9
Kartikey Dev <nikhilkartikey@yahoo.com>
2020-06-28 19:58:54 +0530

    Kartikey_Assgnment_19_Changes_Incorporated

--- src/kartikeyDJune20/FirstDifferIndexArray.java
+++ src/kartikeyDJune20/FirstDifferIndexArray.java
@@ -7,36 +7,20 @@ public class FirstDifferIndexArray {
 		if (input1.length == input2.length) {
 
 			boolean flag = true;
-			int index = 0;
-			for (; index < input1.length; index++) {
+			
+			for (int index = 0; index < input1.length; index++) {
 
 				if (input1[index] != input2[index]) {
 					flag = false;
 					System.out.println("Values are not matching at index: " + index);
-					int max;
-					int diff = 0;
-					if (input1[index] > input2[index]) {
-						max = input1[index];
-						diff = input1[index] - input2[index];
-					} else {
-						max = input2[index];
-						diff = input2[index] - input1[index];
-					}
-					System.out.println("Values are not matching at index: " + index + " From (" + input1[index] + ","
-							+ input2[index] + ") Max value is " + max + " And difference between two is " + diff);
-
 				}
 			}
 			if (flag == true) {
-				System.out.println("both arrays are indentical");
-			} else
-				System.out.println("Bothe arrays are not identical");
-
+				System.out.println("Both arrays are indentical");
+			}
 		} else
-			System.out.println("Provided arrays are  not equal");
-
+			System.out.println("Provided arrays are  not equal in Size");
 	}
-
 	public static void main(String[] args) {
 		FirstDifferIndexArray firstDifferIndexArray = new FirstDifferIndexArray();
 		int[] input1 = { 10, 2, 9, 14, 3 };
