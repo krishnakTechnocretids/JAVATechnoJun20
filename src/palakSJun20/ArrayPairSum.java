eadc0bde306b85193e430be072029d37d5564667
Palak Soni <palaksoni1907@gmail.com>
2020-06-28 11:16:27 +0530

    Update_RepeatationOfInteger_Part

--- src/palakSJun20/ArrayPairSum.java
+++ src/palakSJun20/ArrayPairSum.java
@@ -1,19 +1,23 @@
 package palakSJun20;
 
 public class ArrayPairSum {
-	void findArrayPairSum(int[] array){
+	void findArrayPairSum(int[] array) {
 		System.out.println("Integer numbers, whose sum is equal to value:7");
 		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array.length; j++) {
-				if(array[i] + array[j] == 7 )
-					System.out.println("(" + array[i] +"," + array[j] + ")");
+			if (!(array[i] == '*')) {
+				for (int j = 0; j < array.length; j++) {
+					if (array[i] + array[j] == 7) {
+						System.out.println("(" + array[i] + "," + array[j] + ")");
+						array[j] = '*';
+					}
+				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		ArrayPairSum arrayPairSum = new ArrayPairSum();
-		int[] array = {2, 4, 3, 5, 7, 8, 9 , -1};
+		int[] array = { 2, 4, 3, 5, 7, 8, 9, -1 };
 		arrayPairSum.findArrayPairSum(array);
 	}
 
