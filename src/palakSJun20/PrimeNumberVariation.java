e7088d0826da4e8d5b10363c9d3d791dd6f48a9f
Palak Soni <palaksoni1907@gmail.com>
2020-06-28 23:27:41 +0530

    Changes_Updated

--- src/palakSJun20/PrimeNumberVariation.java
+++ src/palakSJun20/PrimeNumberVariation.java
@@ -6,7 +6,7 @@
 package palakSJun20;
 
 public class PrimeNumberVariation {
-
+		
 		// Given number is prime or not. 
 		void findGivenNumberPrimeOrNot(int number){
 			boolean flag = false;
@@ -24,8 +24,9 @@ public class PrimeNumberVariation {
 		}
 		
 		//Print Total count of prime number b/w 2-100
-		int printTotalCountOfPrimeNumber(){
+		void findPrimeNumber(){
 			int count = 0;
+			int sum = 0;
 			for (int i = 2; i < 100; i++) {
 				boolean flag = false;
 				for (int j = 2; j < i ; j++) {
@@ -36,49 +37,20 @@ public class PrimeNumberVariation {
 				}
 				if(flag == false){
 					count++;
-				}
-			}
-			return count;
-		}
-		
-		//Find sum of all prime numbers between 2->100
-		int findSumOfPrimeNumbers(){
-			int sum = 0;
-			for (int i = 2; i < 100; i++) {
-				boolean flag = false;
-				for (int j = 2; j < i; j++) {
-					if ( i%j == 0) {
-						flag = true;
-						break;
-					}
-				}
-				if (flag == false) {
 					sum = sum + i;
 				}
 			}
-			return sum;
+			System.out.println("Total Count of Prime Number between 1-100 is : "+count +"\n" +"Total Sum of Prime Number is :"+sum);
+			System.out.println("Average of Prime Number is : " + sum/count);
 		}
 		
-		//Find avg of prime numbers between 2->100
-		void findAvgOfPrimeNumber(){
-			System.out.println("Average of Prime Number is : " + findSumOfPrimeNumbers()/printTotalCountOfPrimeNumber());
-		}
-
 		
 	public static void main(String[] args) {
 		PrimeNumberVariation primeNumberVariation = new PrimeNumberVariation();
 		int number = 13;
 		primeNumberVariation.findGivenNumberPrimeOrNot(number);
 		
-		//Calling Count of Prime number Method
-		System.out.println("Total Count of Prine Number is : " +primeNumberVariation.printTotalCountOfPrimeNumber());
-		
-		//calling Sum of Prime Number Method 
-		System.out.println("Total Sum of Prime Number is : "+primeNumberVariation.findSumOfPrimeNumbers());
-		
-		//Calling Average Method 
-		primeNumberVariation.findAvgOfPrimeNumber();
+		primeNumberVariation.findPrimeNumber();
 		
 	}
-
 }
