5e553f718fa91f69af930239d521ea0aef49517a
Aastha Jaiswal <jaiswal.aastha1691@gmail.com>
2020-06-28 11:20:58 +0530

    Aastha_Coding_Exam_3_5mins

--- src/aasthaJJun20/IntegerPairSum.java
+++ src/aasthaJJun20/IntegerPairSum.java
@@ -4,8 +4,8 @@ public class IntegerPairSum {
 	void findPairs(int[] input, int sum){
 		System.out.println("Integer numbers, whose sum is equal to value: "+sum);
 		for(int index=0; index<input.length; index++){
-			for(int innerIndex = 0; innerIndex < input.length; innerIndex++){
-				if(sum == input[index]+input[innerIndex])
+			for(int innerIndex = index+1; innerIndex < input.length; innerIndex++){
+				if(sum == (input[index]+input[innerIndex]))
 					System.out.println("("+input[index]+","+input[innerIndex]+")");
 			}
 		}
