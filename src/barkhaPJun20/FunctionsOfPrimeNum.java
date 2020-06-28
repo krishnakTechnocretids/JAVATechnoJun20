d51cc28c7a747e0606220bea9b30c23df43208e5
Barkha Patle <barkhapatle01@gmail.com>
2020-06-28 20:33:28 +0530

    Barkha_Assignment_19_ChangesDone

--- src/barkhaPJun20/FunctionsOfPrimeNum.java
+++ src/barkhaPJun20/FunctionsOfPrimeNum.java
@@ -4,63 +4,36 @@ public class FunctionsOfPrimeNum {
 	int sum=0;
 	int count=0;
 	
-	void primeOrNot(int num) {
+	boolean primeNumber(int num) {
 		boolean flag=true;    
 		for (int index=2; index<=num/2; index++) {
 			if (num%index==0) {
 				flag=false;  // if flag false then not Prime Num
 				break;
 			}	
-		}	
-		if (flag==true)
-			System.out.println("Number "+num+" is Prime Number.");
+		}if (flag==true)	
+			return true;    //if method returns true then num is Prime
 		else
-			System.out.println("Number "+num+" is Not Prime Number.");
+			return false;
 	}
-	void printCountOfPrimeNum() {
-		//int count=0;
+	void printCountOfPrimeNumInGivenRange(int startNum, int endNum) {
+		int sum=0;
+		int count=0;
 		
-		for (int number=2; number<=100; number++) {
-			boolean flag=true; 
-			for (int index=2; index<=number/2; index++) {
-				if (number%index==0) {
-					flag=false;
-					break;
-				}
-			}if (flag==true)	
-				count++;	
-				sum=sum+number;
-			}		
-		System.out.println("Total count of Prime numbers in the range 2-100 are: "+count);		
-	}
-	void printSumOfPrimeNum() {	
-	
-		System.out.println("Total sum of Prime numbers in the range 2-100 are: "+sum);
-	}
-	void printAvgOfPrimeNum() {
-		float avg = (float)sum/(float)count;
-		System.out.println("Total average of Prime numbers in the range 2-100 is: "+avg);
-	}
-	public static void main(String[] args) {
+		for (int index=startNum; index<=endNum; index++) {
+			boolean flag=primeNumber(index);
+			if (flag==true) {
+				count++;
+				sum=sum+index;
+			}
+		}
+			System.out.println("Total Count of Prime Numbers between range "+startNum +" to "+endNum +" is "+count);
+			System.out.println("Sum of all Prime Numbers between range "+startNum +" to "+endNum +" is "+sum);
+			System.out.println("Average of all Prime Numbers between range "+startNum +" to "+endNum +" is "+((float)sum/(float)count));
+		
+		}
+	public static void main(String[] args) {	
 		FunctionsOfPrimeNum functionsOfPrimeNum=new FunctionsOfPrimeNum();
-		int num=89;
-		functionsOfPrimeNum.primeOrNot(num);
-		functionsOfPrimeNum.printCountOfPrimeNum();
-		functionsOfPrimeNum.printSumOfPrimeNum();
-		functionsOfPrimeNum.printAvgOfPrimeNum();
+		functionsOfPrimeNum.printCountOfPrimeNumInGivenRange(2, 100);
 	}
 }	
-	
-			
-	
-	
-		
-	
-			
-			
-			
-		
-		
-	
-	
-	
