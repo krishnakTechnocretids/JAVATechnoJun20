50c2b0ae24bbd7b7bfb2b7c6d335f37422a28f1d
Aavruti Dalmia <aavruti14@gmail.com>
2020-06-29 10:25:29 +0530

    Aavruti_Assignment_20_ModifiedDuplicateNumber

--- src/aavrutiDJun20/array/DuplicateNumberInArray.java
+++ src/aavrutiDJun20/array/DuplicateNumberInArray.java
@@ -3,13 +3,17 @@ package aavrutiDJun20.array;
 public class DuplicateNumberInArray {
 	//Print Duplicate Number available in Array
 	void duplicateNumberInArr(int[] numberArray) {
+		String str[] = new String[numberArray.length];
+		
 		for(int outerIndex=0;outerIndex<numberArray.length;outerIndex++) {
 			int numberCnt=1;
 			for(int innerIndex=outerIndex+1;innerIndex<numberArray.length;innerIndex++) {
-				if(numberArray[outerIndex]==numberArray[innerIndex])
+				if(numberArray[outerIndex]==numberArray[innerIndex]) {
+					str[innerIndex]="*";
 					numberCnt++;
+				}
 			}
-			if(numberCnt>1)
+			if(numberCnt>1 && str[outerIndex] != "*")
 				System.out.println("Duplicate Number in array is : " + numberArray[outerIndex]);
 		}
 	}
@@ -17,8 +21,8 @@ public class DuplicateNumberInArray {
 	public static void main(String[] args) {
 		DuplicateNumberInArray duplicateNumberInArray = new DuplicateNumberInArray();
 		
-		int[] numberArray = {1,1,2,3,4,4,5,6};
-		System.out.println("Array value is {1,1,2,3,4,4,5,6}\n");
+		int[] numberArray = {1,1,2,3,1,4,4,5,6};
+		System.out.println("Array value is {1,1,2,3,1,4,4,5,6}\n");
 		duplicateNumberInArray.duplicateNumberInArr(numberArray);		
 	}
 }
