a47ced9c2cab37c495d64bdf1c42336592fae608
Aadesh Sharma <aadeshsharma008@gmail.com>
2020-06-29 22:23:16 +0530

    Aadesh_Assignment_20_145min

---
+++ src/aadeshJun20/DuplicateNumber.java
@@ -0,0 +1,22 @@
+package aadeshJun20;
+
+public class DuplicateNumber{
+
+	void reverseDisplay(int[] intArr){
+
+		System.out.print(" Reverse number from back : ");
+		for (int i = 0; i < intArr.length; i++){
+			for (int j = i + 1; j < intArr.length; j++){
+				if (intArr[i] == intArr[j]){
+					System.out.println(intArr[i]);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args){
+		DuplicateNumber duplicate = new DuplicateNumber();
+		int[] intArr = { 8, 3, 7, 6, 5 };
+		duplicate.reverseDisplay(intArr);
+	}
+} 
---
+++ src/aadeshJun20/MissingNumber.java
@@ -0,0 +1,30 @@
+package aadeshJun20;
+
+public class MissingNumber {
+
+	void displayMissingNum(int[] intArr) {
+
+		boolean flag;
+		System.out.println(" Missing Numbers are : ");
+		for (int i=1; i<=10; i++) {
+			flag = true;
+			for (int j=0; j<intArr.length; j++) {
+				if (i == intArr[j]) {
+					flag = true;
+					 break;
+				}
+				flag = false;
+			}
+			if(!flag) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		MissingNumber missingNum = new MissingNumber();
+		int[] intArr = {1,2,5,6,8,9};
+		missingNum.displayMissingNum(intArr);
+	}
+
+} 
