36fce93d91745cfaba0041f95a24a1c36b208f81
Anshu Kumari <anshu.v.kumari02@gmail.com>
2020-06-29 17:17:03 +0530

    Anshu-Assignment-20-changesDone

--- src/anshuKJun20/DuplicateNumber.java
+++ src/anshuKJun20/DuplicateNumber.java
@@ -1,17 +1,18 @@
 package anshuKJun20;
 
 public class DuplicateNumber {
-	int value;
+
 	void findDuplicateNumber(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
+			for (int j = i+1; j < arr.length; j++) {
 				if (arr[i] == arr[j]) {
-					value = arr[i];
-				}
+					System.out.println("Duplicate number in this array is " + arr[i]);
+				       break;
+					}
 			}
 		}
-		System.out.println("Duplicate number in this array is " + value);
 	}
+
 	public static void main(String[] args) {
 		DuplicateNumber duplicatenumber = new DuplicateNumber();
 		int[] arr = { 1, 2, 3, 4, 5, 3 };
