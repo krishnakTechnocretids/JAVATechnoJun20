40da3740b415d75da24eb5e1b785c595c8ac653e
Anshu Kumari <anshu.v.kumari02@gmail.com>
2020-06-29 15:16:21 +0530

    Anshu-Assignment-20-changesDone

--- src/anshuKJun20/DuplicateNumber.java
+++ src/anshuKJun20/DuplicateNumber.java
@@ -1,18 +1,17 @@
 package anshuKJun20;
 
 public class DuplicateNumber {
+	int value;
 	void findDuplicateNumber(int[] arr) {
-		int count = 0;
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr.length; j++) {
 				if (arr[i] == arr[j]) {
-					count = arr[i];
+					value = arr[i];
 				}
 			}
 		}
-		System.out.println("Duplicate number in this array is " + count);
+		System.out.println("Duplicate number in this array is " + value);
 	}
-
 	public static void main(String[] args) {
 		DuplicateNumber duplicatenumber = new DuplicateNumber();
 		int[] arr = { 1, 2, 3, 4, 5, 3 };
--- src/anshuKJun20/ReverseNumber.java
+++ src/anshuKJun20/ReverseNumber.java
@@ -2,22 +2,17 @@ package anshuKJun20;
 
 public class ReverseNumber {
 	int add = 0;
-
 	void dispalyReverseNumber(int number) {
 		int number1 = number;
 		while (number > 0) {
 			int remainder = number % 10;
-			int quotient = number / 10;
-			number = quotient;
+			 number = number / 10;
 			add = add * 10;
 			add += remainder;
-
-		}
+      }
 		System.out.println("Reverse number of " + number1 + " is:" + add);
-
 	}
-
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 		ReverseNumber reversenumber = new ReverseNumber();
 		reversenumber.dispalyReverseNumber(12345);
 	}
