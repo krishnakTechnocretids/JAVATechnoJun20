0fa495a263000dcaa96960b1fbcdaf031247fa95
Shruti_Dubey <shrutidubey69@gmail.com>
2020-06-30 17:53:20 +0530

    Changes done as suggested

--- src/shrutiDJun20Assignments/FibonacciNumbers.java
+++ src/shrutiDJun20Assignments/FibonacciNumbers.java
@@ -3,15 +3,15 @@ package shrutiDJun20Assignments;
 public class FibonacciNumbers {
 
 	void findFibonacciNumber(int num) {
-		int a = 0;
-		int b = 1;
-		int c;
+		int num1 = 0;
+		int num2 = 1;
+		int sumNum;
 		System.out.println("Fibonacci series of first "+num +" numbers is below");
 		for(int i = 1; i <=num;i++){
-			c=a+b;
-			a=b;
-			b=c;
-			System.out.print(a+ "  ");
+			sumNum=num1+num2;
+			num1=num2;
+			num2=sumNum;
+			System.out.print(num1+ "  ");
 		}
 	}
 	public static void main(String[] args) {
