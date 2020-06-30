85e269dbfcc56449dcc44ba824ad2c0c7416800c
Anshu Kumari <anshu.v.kumari02@gmail.com>
2020-06-30 23:37:00 +0530

    Anshu-Assignment-21-30+30 changesDone

--- src/anshuKJun20/FibonacciSeries.java
+++ src/anshuKJun20/FibonacciSeries.java
@@ -1,26 +1,27 @@
 package anshuKJun20;
 
 public class FibonacciSeries {
-	
+
 	private void findFibbonacciSeries(int count) {
-		System.out.println('\n'+"The fibonacci Series upto "+count+":");
-	    int number1 =0 ,number2 = 1;
+		System.out.println('\n' + "The fibonacci Series upto " + count + ":");
+		int number1 = 0, number2 = 1;
 		display(number2);
-	    for(int index=0;index<count;index++) {
-	       int number3 = number1 +   number2;
-			display(number3);
+		for (int index = 1; index < count; index++) {
+			int sum = number1 + number2;
+			display(sum);
 			number1 = number2;
-			number2 = number3;
+			number2 = sum;
 		}
 	}
-	
+
 	void display(int number3) {
-			System.out.print(number3+" ");
-		}
+		System.out.print(number3 + " ");
+	}
+
 	public static void main(String[] args) {
-		FibonacciSeries fibonacciseries  = new FibonacciSeries();
+		FibonacciSeries fibonacciseries = new FibonacciSeries();
 		fibonacciseries.findFibbonacciSeries(5);
 		fibonacciseries.findFibbonacciSeries(10);
 	}
-	
+
 }
--- src/anshuKJun20/SwapNumber.java
+++ src/anshuKJun20/SwapNumber.java
@@ -3,20 +3,20 @@ package anshuKJun20;
 public class SwapNumber {
 
 	private void swapMethod(int number1, int number2) {
-		System.out.println("Input = ("+number1+" "+number2+")");
-		 number1 = number1*number2;
-		 number2 = number1/number2;
-	     number1 = number1/number2;
+		System.out.println("Input = (" + number1 + " " + number2 + ")");
+		number1 = number1 * number2;
+		number2 = number1 / number2;
+		number1 = number1 / number2;
 		display(number1, number2);
 	}
 
 	void display(int number1, int number2) {
-		System.out.println("Output = ("+number1+" "+number2+")");
+		System.out.println("Output = (" + number1 + " " + number2 + ")");
 	}
 
 	public static void main(String[] args) {
-		SwapNumber swapno = new SwapNumber();
-		swapno.swapMethod(10, 20);
-		
+		SwapNumber swapNum = new SwapNumber();
+		swapNum.swapMethod(10, 20);
+
 	}
 }
