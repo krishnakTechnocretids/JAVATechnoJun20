efc46181a4914f71604f5c08ca42efaaea27d018
Barkha Patle <barkhapatle01@gmail.com>
2020-06-30 18:45:07 +0530

    Barkha_Assignment_21_ChangesDone

--- src/barkhaPJun20/SwappingOfNumbers.java
+++ src/barkhaPJun20/SwappingOfNumbers.java
@@ -4,14 +4,14 @@ public class SwappingOfNumbers {
 	//Without using third Variable
 	void SwapNumbers(int num1,int num2) {
 		
-		int sum=num1+num2;               //sum=30
-		num1=sum-num1;                   //num1=30-10
-		num2=sum-num2;                   //num2=30-20
+		num1=num1+num2;                //num1=30
+		num2=num1-num2;                //num2=30-20=10
+		num1=num1-num2;                //num1=30-10
 	
 		System.out.println("Value of variable num1 after Swapping is: "+num1);
 		System.out.println("Value of variable num2 after Swapping is: "+num2);
 	}	
-	//With using third variable
+	//Using third variable
 	void SwapNumbers2(int num1,int num2) {
 		
 		int temp=num1;
