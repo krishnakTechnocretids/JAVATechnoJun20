b32e8aef5e680bd85d4a9108f32ae224a9b9bd6f
Kartikey Dev <nikhilkartikey@yahoo.com>
2020-07-01 18:22:34 +0530

    Changes incorporated

--- src/kartikeyDJune20/PalindromeString.java
+++ src/kartikeyDJune20/PalindromeString.java
@@ -5,7 +5,7 @@ public class PalindromeString {
 	static void checkStringPalindrome(String input) {
 		String rev = "";
 		for (int index = input.length() - 1; index >= 0; index--) {
-			rev = rev + String.valueOf(input.charAt(index));
+			rev = rev + input.charAt(index);
 		}
 		if (rev.equalsIgnoreCase(input))
 			System.out.println("Given string " + input + " is Palindrome");
