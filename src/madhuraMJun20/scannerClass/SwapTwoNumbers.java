b1ca03f689e37be79f267c0aba39af5efa5f6836
Madhura Mulay <madhurah02@gmail.com>
2020-07-01 08:00:48 +0530

    Updated the code

--- src/madhuraMJun20/scannerClass/SwapTwoNumbers.java
+++ src/madhuraMJun20/scannerClass/SwapTwoNumbers.java
@@ -4,16 +4,14 @@ package madhuraMJun20.scannerClass;
 public class SwapTwoNumbers {
 	void swapNumbers(int num1, int num2) {
 		System.out.println("Input -> Number 1 = " + num1 + ", Number 2 = " + num2);
-		num2 = num1 + num2;// 30
-		num1 = num2 - num1;// 20
+		num2 = num1 + num2; // 30
+		num1 = num2 - num1; // 20
 		num2 = num2 - num1;
 		System.out.println("Output -> Number 1 = " + num1 + ", Number 2 = " + num2);
 	}
 
 	public static void main(String[] args) {
 		SwapTwoNumbers swapTwoNumbers = new SwapTwoNumbers();
-		int num1 = 10;
-		int num2 = 20;
-		swapTwoNumbers.swapNumbers(num1, num2);
+		swapTwoNumbers.swapNumbers(10, 20);
 	}
 }
