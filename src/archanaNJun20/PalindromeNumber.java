95f952551ad96d47e3f3d182f4f0380b7d7fe659
Archana Nawale <nawalearchana0@gmail.com>
2020-07-01 21:24:30 +0530

    Archana_Assignment_22_Code_Updated

--- src/archanaNJun20/PalindromeNumber.java
+++ src/archanaNJun20/PalindromeNumber.java
@@ -1,9 +1,10 @@
 package archanaNJun20;
 
 public class PalindromeNumber {
-	int rev=0;
-	void palindromeNumber(int number){
+
+	void printPalindromeNumber(int number){
 		int oNumber=number;
+		int rev=0;
 		while(number>0){
 			int mod=number%10;
 			rev=(rev*10)+mod;
@@ -16,7 +17,7 @@ public class PalindromeNumber {
 	}
 	public static void main(String[] args) {
 		PalindromeNumber palindromeNumber=new PalindromeNumber();
-		palindromeNumber.palindromeNumber(45654);
-		palindromeNumber.palindromeNumber(56789);
+		palindromeNumber.printPalindromeNumber(45654);
+		palindromeNumber.printPalindromeNumber(56789);
 	}
 }
--- src/archanaNJun20/PalindromeString.java
+++ src/archanaNJun20/PalindromeString.java
@@ -1,23 +1,21 @@
 package archanaNJun20;
 
 public class PalindromeString {
-	void palindromeString(String inputString){ 
-		String originalstring=inputString;
+	void printPalindromeString(String inputString){ 
 		int length=inputString.length();
 		String newString = "";
 		for(int index=length-1;index>=0;index--){
 			char ch=inputString.charAt(index);
-			newString=newString+String.valueOf(ch);
+			newString+=String.valueOf(ch);
 		}
-		if(newString.equalsIgnoreCase(originalstring))
-
-			System.out.println("Given String : "+ originalstring +" is palindrome");
+		if(newString.equalsIgnoreCase(inputString))
+			System.out.println("Given String : "+ inputString +" is palindrome");
 		else 
-			System.out.println("Given String : "+ originalstring +" is not palindrome");
+			System.out.println("Given String : "+ inputString +" is not palindrome");
 	}
 	public static void main(String[] args){
 		PalindromeString palindromeString= new PalindromeString();
-		palindromeString.palindromeString("radar");
-		palindromeString.palindromeString("technocredits");
+		palindromeString.printPalindromeString("radar");
+		palindromeString.printPalindromeString("technocredits");
 	}
 }
