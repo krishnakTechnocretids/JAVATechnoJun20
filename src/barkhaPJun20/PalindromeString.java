99de554eedb47909f8ad8b7a9e2503b4a7bb98f0
Barkha Patle <barkhapatle01@gmail.com>
2020-07-01 14:50:46 +0530

    Barkha_Assignment_22_25 mins

---
+++ src/barkhaPJun20/PalindromeString.java
@@ -0,0 +1,22 @@
+package barkhaPJun20;
+
+public class PalindromeString {
+	
+	void palindromeString (String originalName) {
+		String name1=originalName;
+		String temp="";                       //if we do not assign anything here, it will take null and will print nullr,nullra and so on in line 10."" is not a space considered as nothing
+		
+		for (int index=(name1.length()-1); index>=0; index--) {
+			temp=temp+name1.charAt(index);   //String+anything(here character) will be String.
+		}
+		if (temp.equalsIgnoreCase(originalName))
+			System.out.println("Name ("+originalName+") is a Palindrome Name.");
+		else
+			System.out.println("Name ("+originalName+") is Not a Palindrome Name.");
+	}
+	public static void main(String[] args) {
+		PalindromeString palindromeString=new PalindromeString();
+		String name="radar";
+		palindromeString.palindromeString(name);
+	}
+}
