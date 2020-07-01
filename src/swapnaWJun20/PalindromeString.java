a51dbf69a95c351fa9195ea63d5dc2cdc4d9e52f
Swapna Wairagade <wairagadeswapna@gmail.com>
2020-07-01 23:24:12 +0530

    Swapna_Assignment_22_Done_25_min

---
+++ src/swapnaWJun20/PalindromeString.java
@@ -0,0 +1,24 @@
+package swapnaWJun20;
+
+public class PalindromeString {
+
+	void findPalindromeNumber(String input) {
+		String input1 = input;
+		String reverse = "" ;
+
+		for (int index = input.length() - 1; index >= 0; index--) {
+			reverse += input.charAt(index);
+		}
+
+		if (input1.equals(reverse))
+			System.out.println("Input : "+input + " is Palindrome");
+		else
+			System.out.println("Input : "+input + " is not Palindrome");
+	}
+
+	public static void main(String[] args) {
+		PalindromeString palindromeString = new PalindromeString();
+		palindromeString.findPalindromeNumber("level");
+		palindromeString.findPalindromeNumber("technocredits");
+	}
+}
