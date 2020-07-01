0fbcdf694fb2e06988ee37e9bf4c659c31208d66
“ADT” <aditi.gajipara@gmail.com>
2020-07-01 01:33:11 -0700

    ADT_Assignment_22_Done_15_10Mins

---
+++ src/aditiGJUN20/PalindromeString.java
@@ -0,0 +1,31 @@
+/*
+ * Verify given String is palindrome or not. 
+ * Input: radar
+ * Output : Given string radar is palindrome.
+ * Input: technocredits
+ * Output: Given string technocredits is not palindrome.
+*/
+package aditiGJUN20;
+
+public class PalindromeString {
+	boolean isPalindromString(String input) {
+		System.out.println("Input String: "+input);
+		String rev="";
+		for ( int index = input.length() - 1; index >= 0; index-- )
+        rev = rev + input.charAt(index); 
+		
+		if(input.equalsIgnoreCase(rev))
+			return true;
+		else
+			return false;
+    } 
+	
+	public static void main(String[] args) {
+		PalindromeString palindromeString = new PalindromeString();
+		if(palindromeString.isPalindromString("Radar")==true)
+			System.out.println("Input String Is Palindrome");
+		else
+			System.out.println("Input String IS NOT Palindrome");
+		
+	}
+}
