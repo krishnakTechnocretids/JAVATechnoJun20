a772d3654c05c015b0d5fe02bae3bf5de14d0291
Anshu Kumari <anshu.v.kumari02@gmail.com>
2020-07-02 07:38:01 +0530

    Anshu-Assignment-22

--- src/anshuKJun20/PalindromeNumber.java
+++ src/anshuKJun20/PalindromeNumber.java
@@ -1,12 +1,11 @@
 package anshuKJun20;
 
 public class PalindromeNumber {
-	private void findPalindromeNumber(int number) {
+	void findPalindromeNumber(int number) {
 		int number1 = number, add = 0;
 		while (number > 0) {
-			int remainder = number % 10;
+			add = add * 10 + number % 10;
 			number = number / 10;
-			add = add * 10 + remainder;
 		}
 		if (add == number1) {
 			System.out.println("Given number " + number1 + " is palindrome.");
@@ -15,14 +14,9 @@ public class PalindromeNumber {
 		}
 	}
 
-	public void display() {
-		findPalindromeNumber(45654);
-		findPalindromeNumber(56789);
-	}
-
 	public static void main(String[] args) {
 		PalindromeNumber palindromeNum = new PalindromeNumber();
-		palindromeNum.display();
-
+		palindromeNum.findPalindromeNumber(45654);
+		palindromeNum.findPalindromeNumber(56789);
 	}
 }
--- src/anshuKJun20/StringPalindrome.java
+++ src/anshuKJun20/StringPalindrome.java
@@ -1,28 +1,22 @@
 package anshuKJun20;
 
 public class StringPalindrome {
-	
-		private void finfPalindromeString(String name) {
-			String result = "";
-			for (int index = (name.length() - 1); index >= 0; index--) {
-				result = result + name.charAt(index);
-			}
-			if (name.equalsIgnoreCase(result)) {
-				System.out.println("Given string " + name + " is palindrome");
-			} else {
-				System.out.println("Given string " + name + " is not palindrome");
-			}
-		}
 
-		public void display() {
-			finfPalindromeString("Radar");
-			finfPalindromeString("Technocredits");
+	void finfPalindromeString(String name) {
+		String result = "";
+		for (int index = (name.length() - 1); index >= 0; index--) {
+			result = result + name.charAt(index);
 		}
-
-		public static void main(String[] args) {
-			StringPalindrome stringpalindrome = new StringPalindrome();
-			stringpalindrome.display();
+		if (name.equalsIgnoreCase(result)) {
+			System.out.println("Given string " + name + " is palindrome");
+		} else {
+			System.out.println("Given string " + name + " is not palindrome");
 		}
 	}
 
+	public static void main(String[] args) {
+		StringPalindrome stringpalindrome = new StringPalindrome();
+		stringpalindrome.finfPalindromeString("Radar");
+		stringpalindrome.finfPalindromeString("Technocredits");
+	}
 }
