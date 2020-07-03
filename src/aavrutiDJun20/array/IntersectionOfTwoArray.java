4578d560895dcd9d09789eb2dd01195d4b08a4fb
Aavruti Dalmia <aavruti14@gmail.com>
2020-07-03 21:02:10 +0530

    Aavruti_Assignment_23_2hrs

--- src/aavrutiDJun20/array/IntersectionOfTwoArray.java
+++ src/aavrutiDJun20/array/IntersectionOfTwoArray.java
@@ -1,43 +1,61 @@
-/*Find intersection(common elements) of two arrays.
-Input: arr1 -> {10.45, 14.0, 18.35, 88.88, 54.10, 18.35}
-          arr2 -> {17.20, 13.30, 10.45, 18.35, 84.33, 13.30}
-
-Output: 10.45, 18.35*/
-
 package aavrutiDJun20.array;
 
 public class IntersectionOfTwoArray {
-	int numberCnt = 0;
+	int duplicateNumberCnt = 0;
 	
+	//Store count of all the duplicate numbers from two arrays and store it in temporary array
 	double[] findIntersectionOfArray(double[] array1,double[] array2) {
-		double[] tempIntersectionArray = new double[array1.length+array2.length];
-		String[] str = new String[array1.length+array2.length];
+		int numberCnt = 0;
+		int arrayLength = 0;
+		
+		if(array1.length>=array2.length)
+			arrayLength = array1.length;
+		else
+			arrayLength = array2.length;
+		
+		double[] tempIntersectionArray = new double[arrayLength];
+		String[] str = new String[arrayLength];
 		str[0] = "";
-		for(int outerIndex=0;outerIndex<array1.length;outerIndex++) {
-			if(str[outerIndex]!= "*" ) {
-				for(int innerIndex=0;innerIndex<array2.length;innerIndex++) {
+		
+		for(int outerIndex=0;outerIndex<array1.length;outerIndex++) {			
+				for(int innerIndex=0;innerIndex<array2.length-1;innerIndex++) {
 					if(array1[outerIndex] == array2[innerIndex]) {
 						tempIntersectionArray[numberCnt] = array2[innerIndex];
-						str[innerIndex] = "*";
 						numberCnt++;
-						break;
 					}
 				}
-			}
 		}
+		
+		for(int outerIndex=0;outerIndex<numberCnt;outerIndex++) {
+			boolean isNumberDuplicate = false;
+			if(str[outerIndex] != "*") {
+				for(int innerIndex=outerIndex+1;innerIndex<numberCnt;innerIndex++) {
+					if(tempIntersectionArray[outerIndex]==tempIntersectionArray[innerIndex]) {
+						isNumberDuplicate = true;
+						str[innerIndex] = "*";
+					}
+				}
+				if(isNumberDuplicate == true) {
+					tempIntersectionArray[duplicateNumberCnt] = tempIntersectionArray[outerIndex];
+					duplicateNumberCnt++;
+				}
+			}
+		}		
 		return tempIntersectionArray;
 	}
 	
+	//Create new array without 0.0 in the array
 	double[] getIntersectionOfArray(double[] array1,double[] array2) {
 		double[] tempIntersectionOfArray = findIntersectionOfArray(array1, array2);
-		double[] intersectionOfArray =  new double[numberCnt];
+		double[] intersectionOfArray =  new double[duplicateNumberCnt];
 		
 		for(int index=0;index<intersectionOfArray.length;index++) {
 			intersectionOfArray[index] = tempIntersectionOfArray[index];
-		}
+		}		
 		return intersectionOfArray;
 	}
 	
+	//Display intersection of two arrays
 	void display(double[] intersectionOfArray) {
 		System.out.println("Intersection of an Array : ");
 		
@@ -48,8 +66,8 @@ public class IntersectionOfTwoArray {
 	
 	public static void main(String[] args) {
 		IntersectionOfTwoArray intersectionOfTwoArray = new IntersectionOfTwoArray();
-		double[] array1 = {10.45, 14.0, 18.35, 88.88, 54.10, 18.35};
-		double[] array2 = {17.20, 13.30, 10.45, 18.35, 84.33, 13.30};
+		double[] array1 = {10.45, 14.0, 10.45, 18.35, 88.88, 54.10, 18.35, 10.45, 18.35};
+		double[] array2 = {17.20, 13.30, 10.45, 18.35, 84.33, 10.45, 13.30, 18.35};
 		
 		intersectionOfTwoArray.display(intersectionOfTwoArray.getIntersectionOfArray(array1, array2));
 	}
--- src/aavrutiDJun20/array/UnionOfTwoArrays.java
+++ src/aavrutiDJun20/array/UnionOfTwoArrays.java
@@ -1,67 +1,54 @@
 package aavrutiDJun20.array;
 
 public class UnionOfTwoArrays {
-	int numberCnt=0;
-	
-	//Store 2 array value in third temp array
-	double[] findUnionOfArrays(double[] array1,double[] array2) {
-		double[] tempUnionArray = new double[array1.length+array2.length];
-		
-		for(int outerIndex=0;outerIndex<array1.length;outerIndex++) {
+	int numberCnt = 0;
+
+	//Store count of all the unique numbers from two arrays and store it in temporary array
+	double[] findUnionOfArrays(double[] array, double[] tempUnionArray) {
+
+		for (int outerIndex=0; outerIndex<array.length; outerIndex++) {
 			boolean isNumberPresent = true;
-			for(int innerIndex=0;innerIndex<tempUnionArray.length;innerIndex++) {
-				if(tempUnionArray[innerIndex]==array1[outerIndex]) {
-					isNumberPresent=false;
+			for (int innerIndex=0; innerIndex<numberCnt; innerIndex++) {
+				if (tempUnionArray[innerIndex]==array[outerIndex]) {
+					isNumberPresent = false;
 					break;
 				}
 			}
-			if(isNumberPresent) {
-				tempUnionArray[numberCnt] = array1[outerIndex];
+			if (isNumberPresent) {
+				tempUnionArray[numberCnt]=array[outerIndex];
 				numberCnt++;
 			}
 		}
-		
-		for(int outerIndex=0;outerIndex<array2.length;outerIndex++) {
-			boolean isNumberPresent = true;
-			for(int innerIndex=0;innerIndex<tempUnionArray.length;innerIndex++) {
-				if(tempUnionArray[innerIndex]==array2[outerIndex]) {
-					isNumberPresent=false;
-					break;
-				}
-			}
-			if(isNumberPresent) {
-				tempUnionArray[numberCnt] = array2[outerIndex];
-				numberCnt++;
-			}
-		}
-		
 		return tempUnionArray;
 	}
-	
-	//Get data of Union Array and remove 
-	double[] getUnionOfArrays(double[] array1,double[] array2) {
-	    double[] tempUnionArray = findUnionOfArrays(array1,array2);
+
+	//Create new array which doesn't consist of 0.0
+	double[] getUnionOfArrays(double[] tempUnionArray) {
 		double[] unionOfArray = new double[numberCnt];
-		
-		for(int outerIndex=0;outerIndex<unionOfArray.length;outerIndex++) {
+
+		for (int outerIndex = 0; outerIndex<unionOfArray.length; outerIndex++) {
 			unionOfArray[outerIndex] = tempUnionArray[outerIndex];
-		}		
-		
+		}
+
 		return unionOfArray;
 	}
-	
+
+	//Display union of two array
 	void displayUnionArray(double[] unionOfArray) {
 		System.out.println("Union of Two Array : ");
-		for(int outerIndex=0;outerIndex<unionOfArray.length;outerIndex++) {
+		for (int outerIndex = 0; outerIndex < unionOfArray.length; outerIndex++) {
 			System.out.print(unionOfArray[outerIndex] + " ");
-		}				
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		UnionOfTwoArrays unionOfTwoArrays = new UnionOfTwoArrays();
-		double[] array1 = {10.45, 14.0, 18.35, 88.88, 54.10, 18.35};
-		double[] array2 = {17.20, 13.30, 10.45, 18.35, 84.33, 13.30};
-				
-		unionOfTwoArrays.displayUnionArray(unionOfTwoArrays.getUnionOfArrays(array1,array2));
+		double[] array1 = {10.45, 14.0, 18.35, 88.88, 54.10, 18.35, 10.45};
+		double[] array2 = {17.20, 13.30, 10.45, 18.35, 84.33, 13.30, 18.35};
+		double[] tempUnionArray = new double[array1.length + array2.length];
+
+		tempUnionArray = unionOfTwoArrays.findUnionOfArrays(array1, tempUnionArray); 
+		tempUnionArray = unionOfTwoArrays.findUnionOfArrays(array2, tempUnionArray); 
+		unionOfTwoArrays.displayUnionArray(unionOfTwoArrays.getUnionOfArrays(tempUnionArray));
 	}
 }
