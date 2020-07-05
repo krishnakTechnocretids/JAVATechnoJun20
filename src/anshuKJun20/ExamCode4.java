931eef9e487efeceece07078694b646567e5343c
Anshu Kumari <anshu.v.kumari02@gmail.com>
2020-07-05 11:53:12 +0530

    Anshu-Exam-Code-40min

---
+++ src/anshuKJun20/ExamCode4.java
@@ -0,0 +1,33 @@
+package anshuKJun20;
+
+public class ExamCode4 {
+	public static void main(String[] args) {
+		int[] arr = { 10, 33, 55, 43, 99, 21 };
+		int [] arr1 = {100,105, 1,99,32,105};
+		int[] arr2 = {100,21,99,55,43};
+		ExamCode4 exammCode = new ExamCode4();
+		exammCode.findSecondMaximumNumber(arr);
+		exammCode.findSecondMaximumNumber(arr1);
+		exammCode.findSecondMaximumNumber(arr2);
+
+	}
+
+	void findSecondMaximumNumber(int[] arr) {
+		int secMaxNum = 0, maxNum = arr[0];
+		for (int index = 1; index < arr.length; index++) { 
+			if (maxNum < arr[index]) {
+				secMaxNum = maxNum;
+
+				maxNum = arr[index];
+
+			} else {
+				if (secMaxNum < arr[index] && arr[index]!=maxNum) { // this condition is required in case when max number comes before max number.
+					secMaxNum = arr[index];
+				}
+			}
+		}
+		System.out.println(secMaxNum);
+
+	}
+
+}
