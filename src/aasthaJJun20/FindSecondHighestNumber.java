7c026336297aa2ec226c026c3e81be78e7fabdbe
Aastha Jaiswal <jaiswal.aastha1691@gmail.com>
2020-07-06 01:15:07 +0530

    Aastha_Coding_Exam_4_20mins

--- src/aasthaJJun20/FindSecondHighestNumber.java
+++ src/aasthaJJun20/FindSecondHighestNumber.java
@@ -5,19 +5,19 @@ public class FindSecondHighestNumber {
 	int secondMaxNumber(int[] numbers) {
 		int max = numbers[0];
 		int secondMax = numbers[1];
-		for (int index = 2; index < numbers.length; index++) {
+		for (int index = 0; index < numbers.length; index++) {
 			if(numbers[index] > max) {
 				secondMax = max;
 				max = numbers[index];
 			}
-			else if(numbers[index] > secondMax)
+			else if(numbers[index] > secondMax && numbers[index] != max)
 				secondMax = numbers[index];
 		}
 		return secondMax;
 	}
 
 	public static void main(String[] args) {
-		int[] arr = {10,33,56,43,99,21,79};
+		int[] arr = {98,99,56,43,99,21,79};
 		FindSecondHighestNumber findSecondHighestNumber = new FindSecondHighestNumber();
 		System.out.println("second highest number is : "+(findSecondHighestNumber.secondMaxNumber(arr)));
 	}
