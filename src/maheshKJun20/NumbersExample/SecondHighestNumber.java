033dad94f355638ef5151aa06ea7afe43463ed5b
Mahesh Kumavat <mkumavat31@gmail.com>
2020-07-05 11:27:09 +0530

    Updated Code

--- src/maheshKJun20/NumbersExample/SecondHighestNumber.java
+++ src/maheshKJun20/NumbersExample/SecondHighestNumber.java
@@ -3,11 +3,14 @@ package maheshKJun20.NumbersExample;
 public class SecondHighestNumber {
 	
 	void displaySecondHigeshtNumber(int[] intArray) {
-		int highestNum = intArray[0], secondHighestNum =0;
+		int highestNum = 0, secondHighestNum =intArray[0];
 		for (int index=0; index<intArray.length;index++) {
 			if(intArray[index]>highestNum) {
 				secondHighestNum = highestNum;
 				highestNum = intArray[index];
+			}
+			else if (intArray[index]>secondHighestNum) {
+				secondHighestNum = intArray[index];
 			}
 		}
 		System.out.println("Second Highest Number in Array is: " + secondHighestNum);
@@ -15,7 +18,7 @@ public class SecondHighestNumber {
 	
 	public static void main(String[] args) {
 		SecondHighestNumber secondHighestNumber = new SecondHighestNumber();
-		int[] intArray = {10,33,56,43,99,21};
+		int[] intArray =  { 10, 33, 56, 43, 99, 21, 79};
 		secondHighestNumber.displaySecondHigeshtNumber(intArray);
 	}
 }
