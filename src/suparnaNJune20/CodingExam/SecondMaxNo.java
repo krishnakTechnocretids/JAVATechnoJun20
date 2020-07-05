e241c43e360d2811548bcc088da1ce20ff7ceb5e
Suparna Nikam <nikam.suparna.s@gmail.com>
2020-07-05 11:36:10 +0530

    Suparna_Coding_exam04_15Min

---
+++ src/suparnaNJune20/CodingExam/SecondMaxNo.java
@@ -0,0 +1,21 @@
+
+public class SecondMaxNo {
+
+	void fndSecondMaxNo(int number[]) {
+		int max1No = number[0], max2No = number[0];
+		for (int outerIndex = 0; outerIndex < number.length; outerIndex++) {
+			if (number[outerIndex] > max1No && number[outerIndex] > max2No) {
+				max2No = max1No;
+				max1No = number[outerIndex];
+			} else if (number[outerIndex] > max2No && number[outerIndex] < max1No) {
+				max2No = number[outerIndex];
+			}
+		}
+		System.out.println("Second Max no is  =" + max2No);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] number = { 10, 33, 56, 43, 99, 21, 56 };
+		new SecondMaxNo().fndSecondMaxNo(number);
+	}
+}
