374774bf37cfda60c97e9d01b371695c61ab8a2b
Swapna Wairagade <wairagadeswapna@gmail.com>
2020-07-05 11:42:53 +0530

    Swapna_Coding_Exam_Done_15_Min

--- src/swapnaWJun20/SecondMaxValueArray.java
+++ src/swapnaWJun20/SecondMaxValueArray.java
@@ -3,20 +3,23 @@ package swapnaWJun20;
 public class SecondMaxValueArray {
 
 	void findSecondMax(int[] number) {
-		int highest = 0;
-		int secondHighest = 0;
+		int highest = number[0];
+		int secondHighest = number[0];
 
 		for (int index = 0; index < number.length; index++) {
 			if (number[index] > highest) {
 				secondHighest = highest;
 				highest = number[index];
 			}
+			else if(number[index]>secondHighest) {
+				secondHighest = number[index];
+			}
 		}
 		System.out.println(secondHighest);
 	}
 
 	public static void main(String[] args) {
-		int[] number = { 10, 33, 56, 43, 99, 21 };
+		int[] number = { 10, 33, 56, 43, 99, 56 };
 		SecondMaxValueArray secondMaxValueArray = new SecondMaxValueArray();
 		secondMaxValueArray.findSecondMax(number);
 	}
