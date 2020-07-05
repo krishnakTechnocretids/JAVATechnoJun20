a2e80b004fd7ed933f4644ea68eb68b3075517b1
Nikhil M <nikhil.mahabaleshwarkar@gmail.com>
2020-07-05 11:44:28 +0530

    Nikhil_Coding_Exam_4_30Min

---
+++ src/nikhilMJun20/SecondLargest.java
@@ -0,0 +1,23 @@
+package nikhilMJun20;
+
+public class SecondLargest {
+	int findSecondLarest(int[] array) {
+		int temp=0;
+		for(int index=0; index<array.length; index++) {
+			for (int innerIndex = index+1; innerIndex<array.length; innerIndex++) {
+				if(array[index]>array[innerIndex]) {
+					temp = array[index];
+					array[index] = array[innerIndex];
+					array[innerIndex] = temp;
+				}
+			}
+		}
+		return array[1];
+	}
+	
+	public static void main(String[] args) {
+		int[] intArray = {10,33,56,43,99,21};
+		
+		System.out.println("The second-largest number in the given array is: "+new SecondLargest().findSecondLarest(intArray));
+	}
+}
