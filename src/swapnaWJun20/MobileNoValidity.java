f688d1997a30633e46e1243349ef64adf83c4985
Swapna Wairagade <wairagadeswapna@gmail.com>
2020-07-06 23:21:49 +0530

    Swapna_Assignment_24_Done_20_Min

--- src/swapnaWJun20/MobileNoValidity.java
+++ src/swapnaWJun20/MobileNoValidity.java
@@ -2,20 +2,52 @@ package swapnaWJun20;
 
 public class MobileNoValidity {
 
+	boolean flag;
+
+	boolean checkAlphabet(String mobNo) {
+		flag = true;
+		for (int i = 0; i < mobNo.length(); i++) {
+			if (Character.isAlphabetic(mobNo.charAt(i)))
+				flag = false;
+		}
+		return flag;
+	}
+	
 	void checkMobileNo(String mobNo) {
 		if (mobNo.length() > 14 || mobNo.length() < 10) {
+			checkAlphabet(mobNo);
 			System.out.println("\n" + mobNo + " : Mobile no is invalid.");
 		} else {
 			if (mobNo.length() == 10 && mobNo.startsWith("9") || mobNo.startsWith("8") || mobNo.startsWith("7")) {
-				System.out.println("\n" + mobNo + " : Mobile no is valid.");
+				checkAlphabet(mobNo);
+				if (flag == true)
+					System.out.println("\n" + mobNo + " : Mobile no is valid.");
+				else
+					System.out.println("\n" + mobNo + " : Mobile no is invalid.");
 			} else if (mobNo.length() == 11 && mobNo.startsWith("0")) {
-				System.out.println("\n" + mobNo + ": Mobile no is valid.");
+				checkAlphabet(mobNo);
+				if (flag == true)
+					System.out.println("\n" + mobNo + " : Mobile no is valid.");
+				else
+					System.out.println("\n" + mobNo + " : Mobile no is invalid.");
 			} else if (mobNo.length() == 12 && mobNo.charAt(3) == ' ' && mobNo.charAt(7) == ' ') {
-				System.out.println("\n" + mobNo + ": Mobile no is valid.");
+				checkAlphabet(mobNo);
+				if (flag == true)
+					System.out.println("\n" + mobNo + " : Mobile no is valid.");
+				else
+					System.out.println("\n" + mobNo + " : Mobile no is invalid.");
 			} else if (mobNo.startsWith("+91") && mobNo.length() == 14) {
-				System.out.println("\n" + mobNo + ": Mobile no is valid.");
+				checkAlphabet(mobNo);
+				if (flag == true)
+					System.out.println("\n" + mobNo + " : Mobile no is valid.");
+				else
+					System.out.println("\n" + mobNo + " : Mobile no is invalid.");
 			} else {
-				System.out.println("\n" + mobNo + ": Mobile no is invalid.");
+				checkAlphabet(mobNo);
+				if (flag == true)
+					System.out.println("\n" + mobNo + " : Mobile no is valid.");
+				else
+					System.out.println("\n" + mobNo + " : Mobile no is invalid.");
 			}
 		}
 	}
@@ -32,5 +64,6 @@ public class MobileNoValidity {
 		mobileNoValidity.checkMobileNo("0765463742");// invalid
 		mobileNoValidity.checkMobileNo("99897  83742"); // invalid
 		mobileNoValidity.checkMobileNo("998 97 83742");// invalid
+		mobileNoValidity.checkMobileNo("998 979 S7Y2");// invalid
 	}
 }
