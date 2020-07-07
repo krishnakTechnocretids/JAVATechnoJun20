503cee5b88d9605bf356bc0a6a7ccd5f4fd6203d
Barkha Patle <barkhapatle01@gmail.com>
2020-07-07 12:03:37 +0530

    Barkha_Assignment_24_ChangesDone

--- src/barkhaPJun20/VerifyMobileNumber.java
+++ src/barkhaPJun20/VerifyMobileNumber.java
@@ -1,77 +1,54 @@
 package barkhaPJun20;
 
 public class VerifyMobileNumber {
-	
+	private boolean isNumber(String num){
+		for (int index = 0; index < num.length(); index++) {
+			if (Character.isAlphabetic(num.charAt(index)))
+				return false;                                 // if return false then not a number.(include alphabet)
+		}
+		return true;
+	}
 	void validMobileNumber(String num) {
+		System.out.println("Mobile Number provided to validate is: "+num);
 		//Exa.+91 9765463742
 		if (num.length()==14) {
-			if (num.startsWith("+91")&& num.charAt(4)=='9'){
-				boolean flag=true;
-				for (int i=4;i<num.length();i++){
-					if (Character.isAlphabetic(num.charAt(i))){
-						flag=false;
-						break;
-					}
-				}
-				if (flag)
+			if (num.startsWith("+91")&& num.charAt(4)=='9' && isNumber(num.substring(4)) && isNumber(num)) {
+				
 					System.out.println("Number is Valid");
-				else 
+			} else 
 					System.out.println("Number is Invalid");
-			}else
-				System.out.println("Number is Invalid");
+		}	
+		//Exa.09765463742
+		else if (num.length()==11) {
+			if (num.startsWith("0")&& num.charAt(1)=='9' && isNumber(num)) {
+				
+					System.out.println("Number is Valid");
+			} else 
+					System.out.println("Number is Invalid");
 		}
-	//Exa.09765463742
-	if (num.length()==11) {
-		if (num.startsWith("0")&& num.charAt(1)=='9') {
-	
-			boolean flag=true;
-			for (int index=4;index<num.length();index++){
-				if (Character.isAlphabetic(num.charAt(index))){
-					flag=false;
-					break;
-				}
-			}
-			if (flag)
+		//Exa.976 546 3742
+		else if (num.length()==12) {
+			if ((num.startsWith("9"))&&num.charAt(3)==' ' && num.charAt(7)==' ' && isNumber(num.replace(" ", "")))
 				System.out.println("Number is Valid");
 			else
 				System.out.println("Number is Invalid");
-		}else
+		}				
+		//	Exa.9765463742	
+		else if (num.length()==10)  {
+			if (num.startsWith("9") && isNumber(num)) {
+				
+					System.out.println("Number is Valid");
+			} else 
+					System.out.println("Number is Invalid");
+		}
+		//out of Desire Length
+		else 
 			System.out.println("Number is Invalid");
-	}
-	//Exa.976 546 3742
-	if (num.length()==12) {
-		
-		if ((num.startsWith("9"))&&num.charAt(3)==' ' && num.charAt(7)==' ')
-			System.out.println("Number is Valid");
-		else
-			System.out.println("Number is Invalid");
-	}	
-	//	Exa.9765463742	
-	if (num.length()==10)  {
-		if (num.startsWith("9")) {
-			boolean flag=true;
-			for (int index=1;index<num.length();index++){
-				if (Character.isAlphabetic(num.charAt(index))){
-					flag=false;
-					break;
-				}
-			}
-			if (flag)
-				System.out.println("Number is Valid");
-			else 
-				System.out.println("Number is Invalid");
-		}else 
-		System.out.println("Number is Invalid");
-	}	
-	//out of Desire Length
-	if (num.length()>14  || num.length()<10)
-		System.out.println("Number is Invalid");
-	}	
-	public static void main(String[] args) {
+		}	
+public static void main(String[] args) {
 		VerifyMobileNumber  mobileNumber=new VerifyMobileNumber ();
-		String mobileNum="9765463A42";
-		System.out.println("Mobile Number provided to validate is: "+mobileNum);
-		mobileNumber.validMobileNumber(mobileNum);
-		
+		//String mobileNum="+91 9765463742";
+		//System.out.println("Mobile Number provided to validate is: "+mobileNum);
+		mobileNumber.validMobileNumber("907 65 46372");
 	}
-}
+}	
