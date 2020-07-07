f4588d38fb07933aa6b93ece9845c13335dddb1b
Krishna Kanani <kananikrishna427@gmail.com>
2020-07-07 10:25:14 +0530

    Technocredits_Abstraction

---
+++ src/technoCredits/abstraction/Client.java
@@ -0,0 +1,8 @@
+package technoCredits.abstraction;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Manager mng = new Lead();
+	}
+}
---
+++ src/technoCredits/abstraction/Lead.java
@@ -0,0 +1,5 @@
+package technoCredits.abstraction;
+
+public class Lead extends Employee3{
+
+}
