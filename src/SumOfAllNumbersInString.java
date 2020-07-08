e2971ba2db85b2ebb4cc0eed2c90185e4f1bf4b4
Piyush Pathak <piyush.p11@gmail.com>
2020-07-08 15:45:27 +0530

    Piyush_Assignment_25_25Min

---
+++ src/SumOfAllNumbersInString.java
@@ -0,0 +1,16 @@
+class SumOfAllNumbersInString {
+	void findSumOfAllNumbers(String input) {
+		int sum = 0;
+		for (int index = 0; index < input.length(); index++) {
+			if (Character.isDigit(input.charAt(index))) {
+				sum = sum + Integer.parseInt(String.valueOf(input.charAt(index)));
+			}
+		}
+		System.out.println(" Sum of all numbers in String is: " + sum);
+	}
+
+	public static void main(String[] args) {
+		SumOfAllNumbersInString sum = new SumOfAllNumbersInString();
+		sum.findSumOfAllNumbers("J7yu9y8h1h8j4b7j3jjb6");
+	}
+}
