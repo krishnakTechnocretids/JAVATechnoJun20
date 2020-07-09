3204039d5cc086caae8d8ec7e6706ab8b869ea5d
Aavruti Dalmia <aavruti14@gmail.com>
2020-07-09 18:59:30 +0530

    Aavruti_Assignment_26_12mins

---
+++ src/aavrutiDJun20/numbers/SumOfNumbersInString.java
@@ -0,0 +1,34 @@
+//sum all the numbers in given string.
+
+package aavrutiDJun20.numbers;
+
+public class SumOfNumbersInString {
+	
+	int findSumOfNumbers(String input) {
+		System.out.println("Input --> " + input);
+		String tempDigit = "0";
+		int sum=0;
+		for(int index=0;index<input.length();index++) {
+			if(Character.isDigit(input.charAt(index))) {
+				tempDigit += input.charAt(index);
+			}
+			else {
+				sum += Integer.parseInt(tempDigit);
+				tempDigit = "0";
+			}	
+		}
+		if(!tempDigit.equals("0"))
+			sum += Integer.parseInt(tempDigit);
+		return sum;
+	}
+	
+	void displaySumOfNumbers(int sumOfNo) {
+		System.out.println("Sum of all numbers in String is: " + sumOfNo + "\n");
+	}
+	
+	public static void main(String[] args) {
+		SumOfNumbersInString sumOfNumbersInString = new  SumOfNumbersInString();
+		sumOfNumbersInString.displaySumOfNumbers(sumOfNumbersInString.findSumOfNumbers("P71Rr29T2E"));
+		sumOfNumbersInString.displaySumOfNumbers(sumOfNumbersInString.findSumOfNumbers("P6Rr29T2E3"));
+	}
+}
