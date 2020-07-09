5ba582cc1b88cde40cf25003aec216b21357bc07
“ADT” <aditi.gajipara@gmail.com>
2020-07-09 09:27:39 -0700

    Updated code as suggested by Amita

--- src/aditiGJUN20/SumOfDigits.java
+++ src/aditiGJUN20/SumOfDigits.java
@@ -9,28 +9,26 @@ public class SumOfDigits {
 			System.out.println("Null String");
 			return 0;
 		}
-		
 		int digitSum = 0;
-		String temp = "0"; // Earlier tried with "" and null but had thrown exception so settled with 0 as
-							// doing integer sum.
+		String temp = "";
 		for (int index = 0; index < str.length(); index++) {
-			char ch = str.charAt(index);
-			if (Character.isDigit(ch))
-				temp += ch;
+			if (Character.isDigit(str.charAt(index)))
+				temp += str.charAt(index);
 			else {
-				digitSum += Integer.parseInt(temp);
-				temp = "0";
+				if (temp.length() != 0) {
+					digitSum += Integer.parseInt(temp);
+					temp = "";
+				}
 			}
 		}
-		int finalSum = digitSum + Integer.parseInt(temp);
-		if (finalSum == 0 && str.contains("0"))
-			System.out.println("Input String contains only '0' as digit");
-		return finalSum;
+		if (temp.length() != 0) {
+			digitSum += Integer.parseInt(temp);
+		}
+		return digitSum;
 	}
 
 	public static void main(String[] args) {
 		SumOfDigits sumOfDigits = new SumOfDigits();
-		// A19di8t7i3112 , 1Adi22ti0
 		String[] stringArray = { "A19di8t7i3112", "1Adi22ti0", "ADi31T08I", "112233", "000A111", "ADITI", "Ad0i0T0",
 				"" };
 		for (int index = 0; index < stringArray.length; index++) {
