081167d07b33b2d7ca9d3f7db1cd89c363ca850f
Swapna Wairagade <wairagadeswapna@gmail.com>
2020-07-09 22:12:01 +0530

    Swapna_Assignment_26_DOne_25_min

---
+++ src/swapnaWJun20/SumStringArrayNosConsecutive.java
@@ -0,0 +1,34 @@
+/*WAP to sum all the numbers in given string.
+Input: P71Rr29T2E
+Ouput: Sum of all numbers in String is: 102
+*/
+
+package swapnaWJun20;
+
+public class SumStringArrayNosConsecutive {
+	String temp = "";
+	int sum = 0;
+
+	int getSumOfConsecutiveNos(String number) {
+		System.out.println("Input : " + number);
+		for (int index = 0; index < number.length(); index++) {
+			if (Character.isDigit(number.charAt(index))) {
+				temp += String.valueOf(number.charAt(index));
+			} else {
+				if (temp.length() != 0) {
+					sum += Integer.parseInt(temp);
+					temp = "";
+				}
+			}
+		}
+		if (temp.length() != 0) {
+			sum += Integer.parseInt(temp);
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Output : " + new SumStringArrayNosConsecutive().getSumOfConsecutiveNos("P71Rr29T2E"));
+		System.out.println("Output : " + new SumStringArrayNosConsecutive().getSumOfConsecutiveNos("P6Rr29T2E3"));
+	}
+}
