c85e54c26ad5672a34c9f20b88bd35ccd6aa8687
Amita Rout <amitarout11@gmail.com>
2020-07-12 03:06:48 -0400

    AMITA_TreeProgram_Updated

--- src/amitaRJun20/TreeProgram.java
+++ src/amitaRJun20/TreeProgram.java
@@ -4,21 +4,20 @@ public class TreeProgram {
 	
 	void display(int[] input) {
 		
-		int count =0;
+		int count =1;
+		int max =input[0];
 		
-		for(int i =0; i<input.length-1; i++) {
-			int max =input[i];
-			if(input[i+1]>max) {
+		for(int i =0; i<input.length; i++) {			
+			if(input[i]>max) {
 				count++;
+				max = input[i];
 			}
 		}
-		System.out.println(count);
+		System.out.println("Total tree count is "+count);
 	}
 
 	public static void main(String[] args) {
 		int[] array = {3,5,5,11,9,10,13};
 		new TreeProgram().display(array);
-
 	}
-
 }
