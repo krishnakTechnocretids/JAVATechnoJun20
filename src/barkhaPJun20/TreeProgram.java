b240575e8100efc41b7ff7dd3b619fad43abb041
Barkha Patle <barkhapatle01@gmail.com>
2020-07-12 12:26:47 +0530

    Barkha_TreeProgram

--- src/barkhaPJun20/TreeProgram.java
+++ src/barkhaPJun20/TreeProgram.java
@@ -3,18 +3,19 @@ package barkhaPJun20;
 public class TreeProgram {
 	
 	void visibleTrees(int [] arr1) {
-		int count=0;
-		for (int i=0; i<arr1.length-1; i++) {
-			if (arr1[i]<arr1[i+1]) {
+		int count=1;
+		int tallestTree=arr1[0];
+		for (int i=1; i<arr1.length; i++) {
+			if (arr1[i]>tallestTree) {
+				tallestTree=arr1[i];
 				count++;
 			}
-		}System.out.println("Visible Tress are: "+count);
-		
+		}
+		System.out.println("Visible Tress are: "+count);
 	}
 	public static void main(String[] args) {
 		TreeProgram treeProgram=new TreeProgram();
 		int [] arr1={3,5,5,11,9,10,13};
 		treeProgram.visibleTrees(arr1);
-		
-	}
-}
+		}
+}	
