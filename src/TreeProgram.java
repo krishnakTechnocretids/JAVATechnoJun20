1780faf2709e35af4e643dc4ebebae3ec0e9f0d9
VireshJ <jangamviresh@yahoo.com>
2020-07-12 12:07:10 +0530

    Viresh_TreeProgram_updated

--- src/TreeProgram.java
+++ src/TreeProgram.java
@@ -1,22 +1,21 @@
 
 public class TreeProgram {
 	
-	int Max=3, count=1;
+	int Max=10, count=1;
 	void findVisibleTree(int[] arr){
+		int Max=arr[0], count=1;
 		for (int index= 0; index < arr.length; index++) {
 			if (arr[index]> Max) {
 				Max = arr[index];
 				count++;
 			}
-				
 		}
 			System.out.println(count);
 	}
 	
-	
 	public static void main(String[] args) {
 
-		int[] arr= {3,5,5,11,9,10,13};
+		int[] arr= {10,5,5,11,9,10,13};
 		
 		new TreeProgram().findVisibleTree(arr);
 	}
