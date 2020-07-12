c20dad16a7f654d9ef7e18bdedb3493885145546
Aavruti Dalmia <aavruti14@gmail.com>
2020-07-12 12:32:28 +0530

    Aavruti_TreeProgram_ModifiedChanges

--- src/aavrutiDJun20/array/TreeProgram.java
+++ src/aavrutiDJun20/array/TreeProgram.java
@@ -3,11 +3,14 @@ package aavrutiDJun20.array;
 public class TreeProgram {
 	
 	public static void main(String[] args) {
-		int[] arr1 = {3,5,5,13,9,10,11};
-		int count = 0;
-		for(int index=0;index<arr1.length-1;index++) {
-			if(arr1[index] < arr1[index+1])
-				count++;
+		int[] arr1 = {3,5,5,13,9,10,14};
+		int count = 1;
+		int max = arr1[0];
+		for(int index=1;index<arr1.length;index++) {
+			if(arr1[index] > max) {
+				max = arr1[index];
+				count++;				
+			}				
 		}		
 		System.out.println(count);
 	}
