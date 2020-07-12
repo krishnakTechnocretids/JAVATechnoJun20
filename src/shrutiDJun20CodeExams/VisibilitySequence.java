b1e1c33ea673f784bc40be05fc69a72ba81208a3
Shruti_Dubey <shrutidubey69@gmail.com>
2020-07-12 12:04:00 +0530

    Shruti_TreeCode_5mints

--- src/shrutiDJun20CodeExams/VisibilitySequence.java
+++ src/shrutiDJun20CodeExams/VisibilitySequence.java
@@ -1,20 +1,17 @@
 package shrutiDJun20CodeExams;
 
 public class VisibilitySequence {
-	void findVisibleTrees(int[] input){
+	void findVisibleTrees(int[] input) {
 		int count = 0;
-		for(int i = 0; i<input.length-1; i++){
-			if(input[i]<input[i+1]){
-				count = count+1;
+		for (int i = 0; i < input.length - 1; i++) {
+			if (input[i] < input[i + 1]) {
+				count = count + 1;
 			}
 		}
-		
-		System.out.println("Count of visible trees--> "+count);
+		System.out.println("Count of visible trees--> " + count);
 	}
-	
-	
 	public static void main(String[] args) {
-		int input[] = { 3,5,5,11,9,10,13};
+		int input[] = { 3, 5, 5, 11, 9, 10, 13, 14, 16, 1, 1, 100 };
 		VisibilitySequence VisibilitySequence = new VisibilitySequence();
 		VisibilitySequence.findVisibleTrees(input);
 	}
