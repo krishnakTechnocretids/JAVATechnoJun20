9163681b85a47dc05224ab21a9ec8e7998a33192
“ADT” <aditi.gajipara@gmail.com>
2020-07-11 23:57:23 -0700

    Rectified the code

--- src/aditiGJUN20/TreeVisibility.java
+++ src/aditiGJUN20/TreeVisibility.java
@@ -1,20 +1,23 @@
 package aditiGJUN20;
 
+import java.util.Arrays;
+
 public class TreeVisibility {
-	
+
 	void displayTree(int[] tree) {
-		int count=0;
-		for (int i = 0; i < tree.length-1; i++) {
-			if(tree[i]<tree[i+1]) {
-				count++;		
+		int count = 1, max = tree[0];
+		for (int index = 1; index < tree.length; index++) {
+			if (tree[index] > max) {
+				count++;
+				max = tree[index];
 			}
 		}
-		System.out.println(count);
+		System.out.println(Arrays.toString(tree) + "-> " + count);
 	}
-	
+
 	public static void main(String[] args) {
-	TreeVisibility treeVisibility = new TreeVisibility();
-	int[] tree= {3,5,5,11,9,10,13};
-	treeVisibility.displayTree(tree);
+		TreeVisibility treeVisibility = new TreeVisibility();
+		int[] tree = { 3, 5, 5, 11, 9, 99, 10, 13 };
+		treeVisibility.displayTree(tree);
 	}
 }
