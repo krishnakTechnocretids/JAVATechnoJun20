4a57296ba7044fe5fb379c0032166a46fb9fddd5
Pranita Puranik <pranita.mini@gmail.com>
2020-07-12 12:25:39 +0530

    Pranita_TreeProgramDone

---
+++ src/pranitaPJun20/TreeProgram.java
@@ -0,0 +1,23 @@
+package pranitaPJun20;
+
+public class TreeProgram {
+	
+	void findNumberOfVisibleTrees(int[] array) {
+		int temp=0;
+		int count=1;
+		for(int index=0;index<array.length-1;index++) {
+			if((array[index+1]>array[index])&& array[index+1]>temp) {
+				temp=array[index+1];
+				count++;
+			}
+		}
+		System.out.println("Total number of visible trees are:"+count);
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {3,5,5,11,9,10,13};
+		TreeProgram treeProgram = new TreeProgram();
+		treeProgram.findNumberOfVisibleTrees(array);
+	}
+
+}
