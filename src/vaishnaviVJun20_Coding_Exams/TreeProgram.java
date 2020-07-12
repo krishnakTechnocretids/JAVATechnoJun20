e7c21e38c103e2f0d6070f78115f17fbb648ef6a
Vaishnavi Vaidya <vaishnavivaidya06@gmail.com>
2020-07-12 12:30:58 +0530

    Vaishnavi_TreeProgram_6Min

--- src/vaishnaviVJun20_Coding_Exams/TreeProgram.java
+++ src/vaishnaviVJun20_Coding_Exams/TreeProgram.java
@@ -1,22 +1,21 @@
 package vaishnaviVJun20_Coding_Exams;
 
 public class TreeProgram {
-	int count =1;
-	
+
 	void howManyTreesYouCanSee(int[] treeArray) {
-		int max=treeArray[0];
-		for(int index=1;index<treeArray.length;index++) {
-			if(treeArray[index]>max) {
-				max=treeArray[index];
+		int max = treeArray[0], count = 1;
+		for (int index = 1; index < treeArray.length; index++) {
+			if (treeArray[index] > max) {
+				max = treeArray[index];
 				count++;
 			}
 		}
-		System.out.println("Visible Tree count is: " +count);
+		System.out.println("Visible Tree count is: " + count);
 	}
 
 	public static void main(String[] args) {
-		int[] treeArray = {3,5,5,11,9,10,13};
-        new TreeProgram().howManyTreesYouCanSee(treeArray);
+		int[] treeArray = { 3, 5, 5, 11, 9, 10, 13 };
+		new TreeProgram().howManyTreesYouCanSee(treeArray);
 	}
 
 }
