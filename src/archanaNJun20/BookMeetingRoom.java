37ce7e951d41263423ab154100e3aeb5d4a7fbf1
Archana Nawale <nawalearchana0@gmail.com>
2020-07-14 22:24:44 +0530

    Archana_Assisgment_27_45mins_Updated_Code_Done

--- src/archanaNJun20/BookMeetingRoom.java
+++ src/archanaNJun20/BookMeetingRoom.java
@@ -1,7 +1,6 @@
 package archanaNJun20;
 
 public class BookMeetingRoom {
-
 	String meetingRoom;
 	double hours=0.0;
 	boolean nalandaFlag=false;
@@ -17,52 +16,52 @@ public class BookMeetingRoom {
 			nalandaFlag=true;
 		}
 		else{
-			System.out.println("-->The meeting room Nalanda is already booked.");
+			System.out.println("-->The meeting room  is already booked.");
 		}
 	}
 	void bookMeetingRoom(String meetingRoom){
-		if(meetingRoom!="Nalanda"){
-			if(meetingRoom=="Takshashila"&&takshashilaFlag==false){
+		if(!(meetingRoom.equals("Nalanda"))){
+			if(meetingRoom.equals("Takshashila")&&takshashilaFlag==false){
 				this.meetingRoom="Takshashila";
 				hours=1;
 				displayInfo();
 				takshashilaFlag=true;
 			}
-			else if(meetingRoom=="AgraFort" &&agraFortFlag==false){
+			else if(meetingRoom.equals("AgraFort") &&agraFortFlag==false){
 				this.meetingRoom="AgraFort";
 				hours=1;
 				displayInfo();
 				agraFortFlag=true;
 			}
-			else if(meetingRoom=="PratapGadh"&&pratapGadhFlag==false){
+			else if(meetingRoom.equals("PratapGadh")&&pratapGadhFlag==false){
 				this.meetingRoom="PratapGadh";
 				hours=1;
 				displayInfo();
 				pratapGadhFlag=true;
 			}
 			else{
-				System.out.println(meetingRoom +"-->The meeting room Nalanda is already booked.");
+				System.out.println(meetingRoom +"-->The meeting room is already booked.");
 			}
 		}
 		else{
-			System.out.println(meetingRoom +"-->The meeting room Nalanda is already booked.");
+			System.out.println(meetingRoom +" is Invalid meeting Room ");
 		}
 	}
 	void bookMeetingRoom(String meetingRoom,int hours){
-		if(meetingRoom!="Nalanda"){
-			if(meetingRoom=="Takshashila"&&takshashilaFlag==false){
+		if(!meetingRoom.equals("Nalanda")){
+			if(meetingRoom.equals("Takshashila")&&takshashilaFlag==false){
 				this.meetingRoom="Takshashila";
 				this.hours=hours;
 				displayInfo();
 				takshashilaFlag=true;
 			}
-			else if(meetingRoom=="AgraFort"&&agraFortFlag==false){
+			else if(meetingRoom.equals("AgraFort")&&agraFortFlag==false){
 				this.meetingRoom="AgraFort";
 				this.hours=hours;
 				displayInfo();
 				agraFortFlag=true;
 			}
-			else if(meetingRoom=="PratapGadh"&&pratapGadhFlag==false){
+			else if(meetingRoom.equals("PratapGadh")&&pratapGadhFlag==false){
 				this.meetingRoom="PratapGadh";
 				this.hours=hours;
 				displayInfo();
@@ -72,32 +71,40 @@ public class BookMeetingRoom {
 			}
 		}
 		else{
-			System.out.println("-->The meeting room Nalanda is already booked.");
+			System.out.println(meetingRoom +" is Invalid meeting Room ");
 		}
 	}
 	void displayInfo(){
 		System.out.println( meetingRoom+" Meeting room "  + " is booked for " + hours + " hour");
 	}
 	void displayAvilableMeetingRoomForBooking() {
-		System.out.println("\nAvailable meeting Rooms:");
-		if(pratapGadhFlag==false){
-			System.out.println("*PratapGadh metting room is available*");
+
+		if(pratapGadhFlag==true&&agraFortFlag==true&&takshashilaFlag==true&&takshashilaFlag==true)
+		{
+			System.out.println("\nAll meeting rooms are booked.");
 		}
-		if(agraFortFlag==false)	{
-			System.out.println("*AgraFort metting room is available*");
-		}
-		if(nalandaFlag==false){
-			System.out.println("*Nalanda metting room is available*");
-		}
-		if(takshashilaFlag==false){
-			System.out.println("*Takshashila metting room is available*");
+		else {  
+			System.out.println("\nAvailable meeting Rooms:");
+			if(pratapGadhFlag==false){
+				System.out.println("*PratapGadh metting room is available*");
+			}
+			if(agraFortFlag==false)	{
+				System.out.println("*AgraFort metting room is available*");
+			}
+			if(nalandaFlag==false){
+				System.out.println("*Nalanda metting room is available*");
+			}
+			if(takshashilaFlag==false){
+				System.out.println("*Takshashila metting room is available*");
+			}
 		}
 	}
 	public static void main(String[] args) {
-		MeetingRoom meetingRoom=new MeetingRoom();
+		BookMeetingRoom meetingRoom=new BookMeetingRoom();
 		meetingRoom.bookMeetingRoom();
 		meetingRoom.bookMeetingRoom();
-		meetingRoom.bookMeetingRoom("AgraFort");
+		meetingRoom.bookMeetingRoom("Nalanda");
+		meetingRoom.bookMeetingRoom("PratapGadh");
 		meetingRoom.bookMeetingRoom("AgraFort",1);
 		meetingRoom.bookMeetingRoom("Takshashila",2);
 		meetingRoom.displayAvilableMeetingRoomForBooking();
