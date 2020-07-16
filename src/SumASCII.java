d15c05ff51368a8903d06f7b63e3da0923a0051d
Piyush Pathak <piyush.p11@gmail.com>
2020-07-16 23:13:24 +0530

    Piyush_Assignment_28_30Min

---
+++ src/SumASCII.java
@@ -0,0 +1,15 @@
+public class SumASCII {
+	void sumOfNumbers(String input) {
+		int sum = 0;
+		for (int index = 0; index < input.length(); index++) {
+			if (input.charAt(index) >= 48 && input.charAt(index) <= 57) {
+				sum += input.charAt(index) - '0';
+			}
+		}
+		System.out.println(" Sum of all numbers in String is: " + sum);
+	}
+
+	public static void main(String[] args) {
+		new SumASCII().sumOfNumbers("J7u9y8h1h8j4b7j3j6");
+	}
+}
