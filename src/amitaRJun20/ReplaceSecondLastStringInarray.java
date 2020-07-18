05831ee1ab6510c7c4f4c62c384759b39ec77717
Amita Rout <amitarout11@gmail.com>
2020-07-18 15:49:31 -0400

    AMITA_Assignment_30_UdatedWithNegativeCondition

--- src/amitaRJun20/ReplaceSecondLastStringInarray.java
+++ src/amitaRJun20/ReplaceSecondLastStringInarray.java
@@ -8,12 +8,16 @@ package amitaRJun20;
 
 public class ReplaceSecondLastStringInarray {
 	
-	void replacedSecondLastString(String input, String replaceString) {
+	String replacedSecondLastString(String input, String replaceString) {
 		int newLength = input.replaceAll("Globant", "").length();
 		int totalCount = (input.length() - newLength) / replaceString.length();
 		String outputString = "";
 		int count = 0;
 		String[] strArray = input.split(" ");
+		if(totalCount<2) {
+			System.out.println("The String has no second last " +replaceString);
+			return input;
+		}
 		for (int index = 0; index < strArray.length; index++) {
 			if (strArray[index].equals(replaceString)) {
 				count++;
@@ -24,14 +28,19 @@ public class ReplaceSecondLastStringInarray {
 			}
 			outputString += strArray[index]+" ";			
 		}
-		System.out.println("The Output String is :    "+outputString);
+		return outputString;
 	}
 
 	public static void main(String[] args) {
 		ReplaceSecondLastStringInarray replaceSecondLastString = new ReplaceSecondLastStringInarray();
 		String input = "Hi Globant Hello Globant Hi Globant GM Globant Hi Globant";
-		System.out.println("The Input String is :    "+input);
+		System.out.println("The Input String is :   "+input);
 		String replaceString = "Globant";
-		replaceSecondLastString.replacedSecondLastString(input, replaceString);
+		System.out.println("The Output String is :   " + replaceSecondLastString.replacedSecondLastString(input, replaceString));
+		System.out.println();
+		String input1 = "Hi Globant";
+		System.out.println("The Input String is :    "+input1);
+		String replaceString1 = "Globant";
+		System.out.println("The Output String is : " + replaceSecondLastString.replacedSecondLastString(input1, replaceString1));
 	}
 }
