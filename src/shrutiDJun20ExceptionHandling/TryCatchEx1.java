4af0eebf8d370a43bc3d98055f4842a8ba44a8e5
Shruti_Dubey <shrutidubey69@gmail.com>
2020-07-20 22:14:03 +0530

    Updated code

--- src/shrutiDJun20ExceptionHandling/TryCatchEx1.java
+++ src/shrutiDJun20ExceptionHandling/TryCatchEx1.java
@@ -5,30 +5,37 @@ import java.util.Scanner;
 
 public class TryCatchEx1 {
 
-	void findSumOfNumbers() {
+	void findSumOfNumbers(int requiredInput) {
 		Scanner scanner = new Scanner(System.in);
-		int count = 1;
+		int count = 0;
 		int sum = 0;
-		while (count <= 3)
+		int[] numArr = new int[requiredInput];
+
+		while (count < requiredInput) {
+
 			try {
-				System.out.println("Enter Input" + count + ":");
-				int number = scanner.nextInt();
-				if (number >= 0 || number < 0) {
-					count++;
-					sum += number;
-				}
+				System.out.println("Enter Input:");
+				numArr[count] = scanner.nextInt();
+				sum += numArr[count];
+				count++;
 			} catch (InputMismatchException ae) {
 
 				System.out
 						.println("Invalid input. Please enter valid input(input should be a number):");
 				scanner.next();
 			}
-		System.out.println("Sum of given input--> " + sum);
+		}
+		System.out.print("Input entered by user: ");
+		for (int index = 0; index < numArr.length; index++) {
+			System.out.print(numArr[index] + " ");
+		}
+
+		System.out.println("\n" + "Sum of given input--> " + sum);
 		scanner.close();
 	}
 
 	public static void main(String[] args) {
 		TryCatchEx1 tryCatchEx1 = new TryCatchEx1();
-		tryCatchEx1.findSumOfNumbers();
+		tryCatchEx1.findSumOfNumbers(3);
 	}
 }
