3131e766f11e077d316a9d605757f00a579e6f67
Aavruti Dalmia <aavruti14@gmail.com>
2020-07-20 21:11:23 +0530

    Aavruti_Assignment_31_ModifiedChanges

--- src/aavrutiDJun20/numbers/SumOfDigitsUsingExceptionHandling.java
+++ src/aavrutiDJun20/numbers/SumOfDigitsUsingExceptionHandling.java
@@ -4,17 +4,20 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SumOfDigitsUsingExceptionHandling {
-	
+
 	//Get values from user
-	int[] getValues(int totalNumbers){
+	void sumOfDigits(int totalNumbers){
 		Scanner scanner = new Scanner(System.in);
-		int count = 0;
+		int count = 0, sum = 0;
+		String concatNumber ="";
 		int[] array = new int[totalNumbers];
 		while(count<totalNumbers) {
 			try {
 				System.out.println("Please enter number "  + (count+1) + ": ");
 				array[count] = scanner.nextInt();
-				count++;
+				sum += array[count];
+				concatNumber += array[count] + ", ";
+				count++;				
 			}
 			catch(InputMismatchException e) {
 				System.out.println("Please enter Valid Number");
@@ -22,20 +25,9 @@ public class SumOfDigitsUsingExceptionHandling {
 				continue;
 			}
 		}
+		concatNumber = concatNumber.replaceAll(", $", "");
+		System.out.println("Output --> values are " + concatNumber + " and total is " + sum);
 		scanner.close();
-		return array;
-	}
-	
-	//Sum of digits
-	void sumOfDigits(int totalNumbers) {
-		int[] array = getValues(totalNumbers);
-		int sum = 0;
-		System.out.print("\nOutput --> values are ");
-		for(int index=0;index<array.length;index++) {
-			sum += array[index];
-			System.out.print(array[index] + ", ");
-		}		
-		System.out.println("and total is " + sum);
 	}
 
 	public static void main(String[] args) {
