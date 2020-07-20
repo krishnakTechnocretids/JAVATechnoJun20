283bf09b6b2c16b861717a2db6c2ca0bee6128b8
Archana Nawale <nawalearchana0@gmail.com>
2020-07-20 20:53:47 +0530

    Archana_Assignment_31_Updated

--- src/archanaNJun20/PrintNumberException.java
+++ src/archanaNJun20/PrintNumberException.java
@@ -1,23 +1,26 @@
 package archanaNJun20;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-public class PrintNumberException {
-	void sumOfDigits() {
+public class PrintNumberException{
+	void  sumOfDigits(){
 		int[] array = new int[3];
 		int counter = 0, sum = 0;
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter numbers:");
-		while(counter<=2) {
-			try {
+		while(counter<=2){
+			try{
 				array[counter] = scanner.nextInt();
 				sum += array[counter];
 				counter++;
-			}catch(InputMismatchException ie) {
-				System.out.println("Please enter integer numeber");
+			}catch(InputMismatchException ie){
+				System.out.println("Please enter integer number");
 				scanner.next();
 			}
 		}
-		System.out.println("Sum of the integer numbers : "+sum);
+		for(int index=0;index<array.length;index++){
+			System.out.print(array[index] + ", ");
+		}		
+		System.out.println(": Sum of numbers is "+sum);
 		scanner.close();
 	}
 	public static void main(String[] args) {
