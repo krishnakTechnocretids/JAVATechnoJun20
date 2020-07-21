07b5af3f2e9471e1a8f8432d2f7e5ee8c6fc4004
“ADT” <aditi.gajipara@gmail.com>
2020-07-20 22:45:02 -0700

    ADT_Assignment_32_Done_6Mins

--- src/aditiGJUN20/SumOfDigitsInString.java
+++ src/aditiGJUN20/SumOfDigitsInString.java
@@ -9,19 +9,20 @@ package aditiGJUN20;
 
 public class SumOfDigitsInString {
 	int getSumOfNumbers(String input) {
-		int sum=0;
-		String[] strArray=input.split(" ");
+		int sum = 0;
+		String[] strArray = input.split(" ");
 		for (int index = 0; index < strArray.length; index++) {
 			try {
-				sum+=Integer.parseInt(strArray[index]);
+				sum += Integer.parseInt(strArray[index]);
 			} catch (NumberFormatException numberFormatException) {
-				
 			}
 		}
 		return sum;
 	}
+
 	public static void main(String[] args) {
-		String str="I have 5 Years 6 month of experience.";
-		System.out.println("For input String: "+str+"\nAddition of all digits present in string is: "+new SumOfDigitsInString().getSumOfNumbers(str));
+		String str = "I have 5 Years 6 month of experience.";
+		System.out.println("For input String: " + str + "\nAddition of all digits present in string is: "
+				+ new SumOfDigitsInString().getSumOfNumbers(str));
 	}
 }
