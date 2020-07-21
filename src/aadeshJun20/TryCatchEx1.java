f35b0b15235948de9da575de0a75243d8bd399d3
Aadesh Sharma <aadeshsharma008@gmail.com>
2020-07-21 07:46:38 +0530

    Aadesh_Assignment_31_2hr_done

---
+++ src/aadeshJun20/TryCatchEx1.java
@@ -0,0 +1,33 @@
+package aadeshJun20;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TryCatchEx1 {
+
+	void findSumOfNumbers() {
+		Scanner scanner = new Scanner(System.in);
+		int count = 1;
+		int sum = 0;
+		while (count <= 4)
+			try {
+				System.out.println("Enter Input" + count + ":");
+				int number = scanner.nextInt();
+				if (number >= 0 || number < 0) {
+					count++;
+					sum += number;
+				}
+			} catch (InputMismatchException ae) {
+
+				System.out
+						.println("Invalid input. Please enter valid input(input should be a number):");
+				scanner.next();
+			}
+		System.out.println("Sum of given input--> " + sum);
+		scanner.close();
+	}
+
+	public static void main(String[] args) {
+		TryCatchEx1 tryCatchEx1 = new TryCatchEx1();
+		tryCatchEx1.findSumOfNumbers();
+	}
+}
