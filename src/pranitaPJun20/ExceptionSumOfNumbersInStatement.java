4b6a458ed79b1151ba312c6f4d10005157e47778
Pranita Puranik <pranita.mini@gmail.com>
2020-07-21 15:26:25 +0530

    Updated_Assignment_32

--- src/pranitaPJun20/ExceptionSumOfNumbersInStatement.java
+++ src/pranitaPJun20/ExceptionSumOfNumbersInStatement.java
@@ -15,7 +15,7 @@ public class ExceptionSumOfNumbersInStatement {
 			try {
 				sum += Integer.parseInt(array[index]);
 			} catch (NumberFormatException nfe) {
-				System.out.print("");
+				
 			}
 		}
 		System.out.println("Input statement is: " + input);
@@ -25,6 +25,5 @@ public class ExceptionSumOfNumbersInStatement {
 	public static void main(String[] args) {
 		String input = "I have 5 Years 6 month of experience.";
 		new ExceptionSumOfNumbersInStatement().findSumOfNumbers(input);
-		;
 	}
 }
