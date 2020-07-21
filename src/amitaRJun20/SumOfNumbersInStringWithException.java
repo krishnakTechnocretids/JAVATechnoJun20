4f6bcc2ed68aa9a0e6f21b43ae6c0bc9567dc4ba
Amita Rout <amitarout11@gmail.com>
2020-07-21 11:05:18 -0400

    AMITA_Assignment_32_Done_10mins

---
+++ src/amitaRJun20/SumOfNumbersInStringWithException.java
@@ -0,0 +1,26 @@
+/*Assignment 32 :   21st July 2020
+WAP to do sum of numbers in given statement. (Use split method and Exception handling concept.
+ Do not use character class methods and ascii concept.) 
+Input : I have 5 Years 6 month of experience.
+Output : 11*/
+package amitaRJun20;
+
+public class SumOfNumbersInStringWithException {
+	
+	int getSumOfNumbersFromString(String inputString){
+		int sum = 0;
+		String[] s = inputString.split(" ");
+		for(int index=0; index<s.length; index++){
+			try{
+				sum += Integer.parseInt(s[index]);			
+			}catch(NumberFormatException ne){
+			}
+		}	
+		return sum;		
+	}	
+	public static void main(String[] args) {
+		String inputString = "I have 5 Years 6 month of experience";
+		System.out.println("The given statement is : "+inputString);
+		System.out.println("Sum of numbers in given statement is :"+new SumOfNumbersInStringWithException().getSumOfNumbersFromString(inputString));
+	}
+}
