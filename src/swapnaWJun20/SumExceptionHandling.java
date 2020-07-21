0ee9a57c613ece5a1838aa9c6b1177fa660e1fd5
Swapna Wairagade <wairagadeswapna@gmail.com>
2020-07-21 08:15:59 +0530

    Swapna_Assignment_31_Suggested_changes_Done_40_min

--- src/swapnaWJun20/SumExceptionHandling.java
+++ src/swapnaWJun20/SumExceptionHandling.java
@@ -9,18 +9,19 @@ public class SumExceptionHandling {
 		int numbers = 0;
 		int sum = 0;
 		int count = 0;
-	
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("How many numbers you want to add : ");
-		int totalNo = sc.nextInt();	
+		int totalNo = sc.nextInt();
 		while (count < totalNo) {
 			try {
-				System.out.println("Enter the number to add : ");
+				System.out.println("\nEnter the number at " + (count + 1) + " to add : ");
 				numbers = sc.nextInt();
 				sum += numbers;
 				count++;
 			} catch (InputMismatchException ie) { //
-				sc.next();
+				System.out.println("Invalid input provided, Please add numbers only");
+				sc.nextLine();
 			}
 		}
 		System.out.println("Total sum of user given numbers is : " + sum);
@@ -28,10 +29,7 @@ public class SumExceptionHandling {
 		return sum;
 	}
 
-	
 	public static void main(String[] args) {
-
-		SumExceptionHandling sumExceptionHandling = new SumExceptionHandling();
-		sumExceptionHandling.getSumThreeNos();
+		new SumExceptionHandling().getSumThreeNos();
 	}
 }
