55d31b760b0e0faf67b6e72ebc508fbb83b078eb
Mahesh Kumavat <mkumavat31@gmail.com>
2020-07-21 19:27:57 +0530

    Update a missing conmdition

--- src/maheshKJun20/StringExamples/SumOfDigitsInStringUsingException.java
+++ src/maheshKJun20/StringExamples/SumOfDigitsInStringUsingException.java
@@ -4,19 +4,24 @@ public class SumOfDigitsInStringUsingException {
 	
 	void displaySumOfDigits(String str) {
 		System.out.println("Input: " + str );
-		String[] tempArray = str.split(" ");
+		String[] tempArray = str.split("");
+		String temp ="0";
 		int sum = 0;
 		for(int index=0;index<tempArray.length;index++) {
 			try {
-				sum += Integer.parseInt(tempArray[index]);
+				temp += "" + Integer.parseInt(tempArray[index]);
 			}catch(NumberFormatException ne) {
-				//ne.printStackTrace();
+				sum += Integer.parseInt(temp);
+				temp = "0";
 			}
 		}
+		if(!temp.equals("0"))
+			sum += Integer.parseInt(temp);
 		System.out.println("Output: Sum of Digits in statement is: " + sum);
 	}
 	public static void main(String[] args) {
 		SumOfDigitsInStringUsingException sumOfDigitsInStringUsingException = new SumOfDigitsInStringUsingException();
 		sumOfDigitsInStringUsingException.displaySumOfDigits("I have 5 Years 6 month of experience.");
+		sumOfDigitsInStringUsingException.displaySumOfDigits("I have 5 Years 6 month of experience. This is my 5th Company");
 	}
 }
