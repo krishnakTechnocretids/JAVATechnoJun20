42454d48c8cddb0da87db8514fcd7be37d17e23f
Mahesh Kumavat <mkumavat31@gmail.com>
2020-07-22 19:33:30 +0530

    Updated as per the new requirement to handle decimal number

--- src/maheshKJun20/StringExamples/SumOfDigitsInStringUsingException.java
+++ src/maheshKJun20/StringExamples/SumOfDigitsInStringUsingException.java
@@ -6,22 +6,29 @@ public class SumOfDigitsInStringUsingException {
 		System.out.println("Input: " + str );
 		String[] tempArray = str.split("");
 		String temp ="0";
-		int sum = 0;
+		double sum = 0;
 		for(int index=0;index<tempArray.length;index++) {
 			try {
-				temp += "" + Integer.parseInt(tempArray[index]);
+				if (tempArray[index].equals("."))
+					temp += tempArray[index];
+				else
+					temp += "" + Integer.parseInt(tempArray[index]);
 			}catch(NumberFormatException ne) {
-				sum += Integer.parseInt(temp);
+				sum += Double.parseDouble(temp);
 				temp = "0";
 			}
 		}
 		if(!temp.equals("0"))
-			sum += Integer.parseInt(temp);
+			sum += Double.parseDouble(temp);
 		System.out.println("Output: Sum of Digits in statement is: " + sum);
 	}
+	
 	public static void main(String[] args) {
 		SumOfDigitsInStringUsingException sumOfDigitsInStringUsingException = new SumOfDigitsInStringUsingException();
 		sumOfDigitsInStringUsingException.displaySumOfDigits("I have 5 Years 6 month of experience.");
-		sumOfDigitsInStringUsingException.displaySumOfDigits("I have 5 Years 6 month of experience. This is my 5th Company");
+		sumOfDigitsInStringUsingException.displaySumOfDigits("I have 5 Years experience");
+		sumOfDigitsInStringUsingException.displaySumOfDigits("I have 5.6 Years experience");
+		sumOfDigitsInStringUsingException.displaySumOfDigits("I have 2.3 Years experience in manual & 1.2 years in automation");
+		sumOfDigitsInStringUsingException.displaySumOfDigits("2.3 Years total experience and this is my 23rd org");
 	}
 }
