a6676562f265c9f046064cf7f88acbda2bf4f055
Amita Rout <amitarout11@gmail.com>
2020-07-22 15:59:32 -0400

    AMITA_Assignment_33_SmallChangesDone

--- src/amitaRJun20/ExtractItemIdFromString.java
+++ src/amitaRJun20/ExtractItemIdFromString.java
@@ -1,8 +1,18 @@
+/*Extract ItemId from below input
+String input = https://www.facebook.com/technocredits/temId=12345/
+String input = https://www.facebook.com/technocredits/empid=123&itemId=12345/
+String input = https://www.facebook.com/technocredits/itemId=12345&empid=123/
+String input = https://www.facebook.com/technocredits/empid=123/
+Output : 
+item id is 12345 
+        OR
+no item id present.*/
 package amitaRJun20;
 
 public class ExtractItemIdFromString {
+
 	void display(String input) {
-		System.out.println("The input String is :"+input);
+		System.out.println("The input String is :" + input);
 		String itemId = "";
 		if (input.contains("itemId")) {
 			String temp = input.substring(input.indexOf("itemId"));
@@ -20,11 +30,11 @@ public class ExtractItemIdFromString {
 			System.out.println("ItemId is not present");
 		}
 	}
+
 	public static void main(String[] args) {
 		new ExtractItemIdFromString().display("https://www.facebook.com/technocredits/itemId=12345/");
 		new ExtractItemIdFromString().display("https://www.facebook.com/technocredits/empid=123&itemId=12345/");
 		new ExtractItemIdFromString().display("https://www.facebook.com/technocredits/itemId=12345&empid=123/");
-		new ExtractItemIdFromString().display("https://www.facebook.com/technocredits/empid=123/");		
+		new ExtractItemIdFromString().display("https://www.facebook.com/technocredits/empid=123/");
 	}
-
 }
