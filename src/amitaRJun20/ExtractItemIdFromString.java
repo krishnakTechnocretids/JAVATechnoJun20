7238530eb016720719a760c5bb98498f5ec6c905
Amita Rout <amitarout11@gmail.com>
2020-07-22 15:48:56 -0400

    AMITA_Assignment_33_Done_20mins

---
+++ src/amitaRJun20/ExtractItemIdFromString.java
@@ -0,0 +1,30 @@
+package amitaRJun20;
+
+public class ExtractItemIdFromString {
+	void display(String input) {
+		System.out.println("The input String is :"+input);
+		String itemId = "";
+		if (input.contains("itemId")) {
+			String temp = input.substring(input.indexOf("itemId"));
+			for (int index = 0; index < temp.length(); index++) {
+				if (Character.isDigit(temp.charAt(index))) {
+					itemId += temp.charAt(index);
+					if (!Character.isDigit(temp.charAt(index + 1))) {
+						break;
+					}
+				}
+			}
+			System.out.println("ItemId is " + itemId);
+			System.out.println();
+		} else {
+			System.out.println("ItemId is not present");
+		}
+	}
+	public static void main(String[] args) {
+		new ExtractItemIdFromString().display("https://www.facebook.com/technocredits/itemId=12345/");
+		new ExtractItemIdFromString().display("https://www.facebook.com/technocredits/empid=123&itemId=12345/");
+		new ExtractItemIdFromString().display("https://www.facebook.com/technocredits/itemId=12345&empid=123/");
+		new ExtractItemIdFromString().display("https://www.facebook.com/technocredits/empid=123/");		
+	}
+
+}
