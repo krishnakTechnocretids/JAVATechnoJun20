8f7b58b53da0ca58407e1238c17c6e05d2cb2ac5
Archana Nawale <nawalearchana0@gmail.com>
2020-07-22 23:28:29 +0530

    Archana_Assignement_33_Updated_code

--- src/archanaNJun20/ExtractItemId.java
+++ src/archanaNJun20/ExtractItemId.java
@@ -1,12 +1,12 @@
 package archanaNJun20;
 
-public class ExtractItemId {
-	void printItemId(String input) {
+public class ExtractItemId{
+	void printItemId(String input){
 		String output = "";
 		if (input.contains("itemId")) {
-			String temp = input.substring(input.indexOf("12345"));
-			for (int index = 0; index < temp.length(); index++) {
-				if (Character.isDigit(temp.charAt(index))) {
+			String temp = input.substring(input.indexOf("itemId"));
+			for (int index = 0; index < temp.length(); index++){
+				if (Character.isDigit(temp.charAt(index))){
 					output += temp.charAt(index);
 					if (!Character.isDigit(temp.charAt(index + 1)))
 						break;
@@ -16,13 +16,13 @@ public class ExtractItemId {
 		} else
 			System.out.println("Item Id is not present");
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		ExtractItemId extractItemId = new ExtractItemId();
 		String[] input = { "https://www.facebook.com/technocredits/temId=12345/",
 				"https://www.facebook.com/technocredits/empid=123&itemId=12345/",
 				" https://www.facebook.com/technocredits/itemId=12345&empid=123/",
 		"https://www.facebook.com/technocredits/empid=123/" };
-		for (int index = 0; index < input.length; index++) {
+		for (int index = 0; index < input.length; index++){
 			extractItemId.printItemId(input[index]);
 		}
 	}
