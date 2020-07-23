973c2cae813c8b6a82b5d0308855a1cce3682216
Palak Soni <palaksoni1907@gmail.com>
2020-07-23 10:26:37 +0530

    Palak_Assingment33_50Min_Done

---
+++ src/palakSJun20/ExtractItemIdFromUrl.java
@@ -0,0 +1,50 @@
+/*Extract ItemId from below input
+
+	String input = https://www.facebook.com/technocredits/temId=12345/
+	String input = https://www.facebook.com/technocredits/empid=123&itemId=12345/
+	String input = https://www.facebook.com/technocredits/itemId=12345&empid=123/
+	String input = https://www.facebook.com/technocredits/empid=123/
+
+	Output : 
+	item id is 12345 
+	        OR
+	no item id present.*/
+package palakSJun20;
+
+public class ExtractItemIdFromUrl {
+	void findIdFromUrl(String input) {
+		String output = "";
+		if (input.contains("itemId")) {
+			for (int index = input.indexOf("itemId"); index < input.length(); index++) {
+				if (input.charAt(index) >= '0' && input.charAt(index) <= '9') {
+					output += input.charAt(index);
+					if (!(input.charAt(index + 1) >= '0' && input.charAt(index) <= '9')) {
+						break;
+					}
+				}
+			}
+			System.out.println("Item id is :" + output);
+		} else
+			System.out.println("Item id Not Found");
+	}
+
+	public static void main(String[] args) {
+		ExtractItemIdFromUrl extractIdFromUrl = new ExtractItemIdFromUrl();
+
+		String input1 = "https://www.facebook.com/technocredits/itemId=12345/";
+		System.out.println("String Input : " + input1);
+		extractIdFromUrl.findIdFromUrl(input1);
+
+		String input2 = "https://www.facebook.com/technocredits/empid=123&itemId=12345/";
+		System.out.println("\nString Input : " + input2);
+		extractIdFromUrl.findIdFromUrl(input2);
+
+		String input3 = "https://www.facebook.com/technocredits/itemId=12345&empid=123/";
+		System.out.println("\nString Input :" + input3);
+		extractIdFromUrl.findIdFromUrl(input3);
+
+		String input4 = "https://www.facebook.com/technocredits/empid=123/";
+		System.out.println("\nString Input :" + input4);
+		extractIdFromUrl.findIdFromUrl(input4);
+	}
+}
