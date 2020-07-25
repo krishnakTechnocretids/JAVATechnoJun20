e9fe7ffffbc20dacc4b476b18b0b38da9a5cde6d
VireshJ <jangamviresh@yahoo.com>
2020-07-25 11:43:44 +0530

    Viresh_Coding_Exam_5_secondRequirement_Added_6Mins

--- src/codingExam5/PatternPrint.java
+++ src/codingExam5/PatternPrint.java
@@ -11,7 +11,17 @@ public class PatternPrint {
 		}
 	}
 
+	void printPattern1(String str) {
+		for (int outerIndex = 0; outerIndex < str.length(); outerIndex++) {
+			for (int innerIndex = outerIndex; innerIndex < str.length(); innerIndex++) {
+				System.out.print(str.charAt(innerIndex));
+			}
+			System.out.println("");
+		}
+	}
+	
 	public static void main(String[] args) {
-		new PatternPrint().printPattern("Globant");
+		//new PatternPrint().printPattern("Globant");
+		new PatternPrint().printPattern1("Globant");
 	}
 }
