94b4f8ede7e8f6e7005fc36092ef959a1c8a7518
Aastha Jaiswal <jaiswal.aastha1691@gmail.com>
2020-07-25 11:46:32 +0530

    Aastha_Codeing_Exam_5_2ndType

--- src/aasthaJJun20/PrintReducedString.java
+++ src/aasthaJJun20/PrintReducedString.java
@@ -6,9 +6,16 @@ public class PrintReducedString {
 			System.out.println(input.substring(0, (input.length() - index)));
 		}
 	}
+	
+	void displayReverseReducedSTring(String input) {
+		for (int index = 0; index < input.length(); index++) {
+			System.out.println(input.substring(index,input.length()-1));
+		}
+	}
 
 	public static void main(String[] args) {
 		PrintReducedString printReducedString = new PrintReducedString();
 		printReducedString.displayReducedSTring("Globant");
+		printReducedString.displayReverseReducedSTring("Globant");
 	}
 }
