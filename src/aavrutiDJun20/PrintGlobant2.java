d08f17364670c0ed367989f8eba3fee4e1a78691
Aavruti Dalmia <aavruti14@gmail.com>
2020-07-25 11:55:31 +0530

    Aavruti_Coding_Exam_6_5mins

--- src/aavrutiDJun20/PrintGlobant2.java
+++ src/aavrutiDJun20/PrintGlobant2.java
@@ -6,7 +6,13 @@ public class PrintGlobant2 {
 		int num = 0;
 		for(int index = input.length();index >= 0; index--) {
 			for(int index2 = num; index2<input.length();index2++) {
-				System.out.print(input.charAt(index2));
+				if(index2 == num){
+					char ch = input.charAt(index2);
+					System.out.print(Character.toUpperCase(ch));
+				}
+				else {
+					System.out.print(input.charAt(index2));
+				}
 			}
 			System.out.println();
 			num++;
