59ad1a76db39146049b8306b27dd1dbb788e6afb
Pranita Puranik <pranita.mini@gmail.com>
2020-07-25 12:31:20 +0530

    Coding_Exam_6_updatedAsPerRequirement

--- src/pranitaPJun20/Coding_Exam_6.java
+++ src/pranitaPJun20/Coding_Exam_6.java
@@ -1,7 +1,19 @@
+/*
+ * Print first letter capital-Using substring method
+Globant
+Lobant
+Obant
+Bant
+Ant
+Nt
+T
+
+ */
 package pranitaPJun20;
 
 public class Coding_Exam_6 {
-		void printStringPattern(String input) {
+		/*void printStringPattern(String input) {
+			System.out.println("\nPattern Print using loop");
 			for (int outerIndex = 0; outerIndex < input.length(); outerIndex++) {
 				for (int innerIndex = outerIndex; innerIndex < input.length() ; innerIndex++) {
 					System.out.print(input.charAt(innerIndex));
@@ -11,16 +23,27 @@ public class Coding_Exam_6 {
 		}
 	
 		void printStringPatternUsingSubstring(String input) {
-			System.out.println("\nUsing substring method");
+			System.out.println("\nPattern Print Using substring method");
 			for (int index = 0; index < input.length(); index++) {
 				System.out.println(input.substring(0+index));
+				
 			}
-		}
+		}*/
 		
+	    void printStringPattrenFirstCapitalLetter(String input) {
+	    	System.out.println("\nPrint first letter capital-Using substring method");
+				for(int index=0; index < input.length(); index++) {
+					String tempInput = input.substring(index, input.length());
+					tempInput = tempInput.replace((tempInput.charAt(0)), Character.toUpperCase(tempInput.charAt(0)));
+					System.out.println(tempInput);
+				}
+			}
+	    
 	public static void main(String[] args) {
 		String input ="Globant";
 		Coding_Exam_6 coding_Exam_6 = new Coding_Exam_6();
-		coding_Exam_6.printStringPattern(input);
-		coding_Exam_6.printStringPatternUsingSubstring(input);
+		//coding_Exam_6.printStringPattern(input);
+		//coding_Exam_6.printStringPatternUsingSubstring(input);
+		coding_Exam_6.printStringPattrenFirstCapitalLetter(input);
 	}
 }
