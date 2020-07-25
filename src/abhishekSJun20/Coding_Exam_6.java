28ed47a473bc5d6c397cc5b99fa2a88a76163a9d
“Abhishek <Abhishek.satyam10@gmail.com>
2020-07-25 12:23:21 +0530

    Abhishek_coding_Exam_06_12minutes

---
+++ src/abhishekSJun20/Coding_Exam_6.java
@@ -0,0 +1,21 @@
+package abhishekSJun20;
+
+public class Coding_Exam_6 {
+	void printString2(String input) {
+				for (int outerIndex = 0; outerIndex < input.length(); outerIndex++) {
+					for (int innerIndex = outerIndex; innerIndex < input.length(); innerIndex++) {
+						if(innerIndex == outerIndex)
+							System.out.print(Character.toUpperCase(input.charAt(innerIndex)) );
+						else
+						System.out.print(input.charAt(innerIndex));
+					}
+					System.out.println("");
+				}
+			}
+		public static void main(String[] args) {
+			String input = "Globant";
+			new Coding_Exam_6().printString2(input);
+		}
+	}
+
+
