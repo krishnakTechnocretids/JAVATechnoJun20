840a47778be66ac653e08b7e228be960f6e3c40e
Vaishnavi Vaidya <vaishnavivaidya06@gmail.com>
2020-07-25 12:17:07 +0530

    Added some changes for capital letters

--- src/vaishnaviVJun20_Coding_Exams/Pattern2.java
+++ src/vaishnaviVJun20_Coding_Exams/Pattern2.java
@@ -3,10 +3,12 @@ package vaishnaviVJun20_Coding_Exams;
 public class Pattern2 {
 	void printGivenPattern(String str) {
 		for (int index = 0; index < str.length(); index++) {
-			System.out.println(str.substring(0+index));
-		}
+			String caps = str.substring(0+index);
+			String str1 = str.substring(1+index);			
+			System.out.println(Character.toUpperCase(caps.charAt(0)) + str1);
+			
 	}
-
+}
 	public static void main(String[] args) {
 		new Pattern2().printGivenPattern("Globant");
 	}
