6982b5d3ef4d6375cff31342d0de2d11e1af9f0d
Rachana Ghayal <rach.g14@gmail.com>
2020-07-25 12:18:43 +0530

    Rachana_Coading_exam_6_15min

--- src/rachanaGJun20/Exam6.java
+++ src/rachanaGJun20/Exam6.java
@@ -3,9 +3,13 @@ package rachanaGJun20;
 public class Exam6 {
 
 	void display(String str) {
+		String temp;
 		int index=0;
+		
 		while(str.length()>1 && index< str.length()) {
-			System.out.println(str.substring(index,str.length()));
+			temp = str.substring(index,str.length());
+			temp = temp.replace(String.valueOf(temp.charAt(0)), String.valueOf(temp.charAt(0)).toUpperCase());
+			System.out.println(temp);
 			index++;
 		}
 	}
