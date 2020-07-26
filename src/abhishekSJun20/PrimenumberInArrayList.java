5f0a5df557a8f685e180d70f5b557cc962b13607
“Abhishek <Abhishek.satyam10@gmail.com>
2020-07-26 12:23:31 +0530

    Abhishek_Coding_Exam_8_20minutes

---
+++ src/abhishekSJun20/PrimenumberInArrayList.java
@@ -0,0 +1,27 @@
+package abhishekSJun20;
+
+import java.util.ArrayList;
+
+public class PrimenumberInArrayList {
+	void findPrimeNumbersUsingArrayList() {
+		ArrayList<Integer> prime = new ArrayList<Integer>();
+		for (int number = 2; number <= 100; number++) {
+			boolean flag = true;
+			for (int index = 2; index <= number / 2; index++) {
+				if (number % index == 0) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag == true) {
+				prime.add(number);
+			}
+		}
+		System.out.println("Array lis of Prime numbers between 1 to 100 : " + prime);
+	}
+
+	public static void main(String[] args) {
+		new PrimenumberInArrayList().findPrimeNumbersUsingArrayList();
+	}
+
+}
