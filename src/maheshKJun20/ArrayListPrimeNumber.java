bb45c4054ab524b774f5035c2cb7e44f65cd7d99
Mahesh Kumavat <mkumavat31@gmail.com>
2020-07-26 11:53:54 +0530

    Updated the code to parametize the function with start and end range

--- src/maheshKJun20/ArrayListPrimeNumber.java
+++ src/maheshKJun20/ArrayListPrimeNumber.java
@@ -9,15 +9,22 @@ import java.util.ArrayList;
 
 public class ArrayListPrimeNumber {
 	
-	void displaylistOfPrimeNumber() {
+	//Method to print list of prime numbers between start and end range
+	void displaylistOfPrimeNumber(int startRange, int endRange) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		for (int index=2;index<=100;index++) {
-			if(primeNumber(index) > 0 ) {
-				list.add(index);
+		if (startRange < endRange && startRange >=2) {
+			for (int index=startRange;index<=endRange;index++) {
+				if(primeNumber(index) > 0 ) {
+					list.add(index);
+				}
 			}
+			System.out.println("Prime Number between " + startRange + " to " + endRange + " are: " + list);
+		}else {
+			System.out.println("Please provide valid range to search prime number \nHints: \n1. StartRange should be greater than 2 \n2. StartRange should be less than EndRange");
 		}
-		System.out.println("Prime Number between 1 to 100 are: " + list);
 	}
+	
+
 	int primeNumber(int num) {
 		//Method to find if Number is Prime Number or not. It return num - If prime and returns 0 - If not Prime
 		for (int index=2; index<=Math.sqrt(num);index++) {
@@ -30,8 +37,6 @@ public class ArrayListPrimeNumber {
 	
 	public static void main(String[] args) {
 		ArrayListPrimeNumber arrayListPrimeNumber = new ArrayListPrimeNumber();
-		arrayListPrimeNumber.displaylistOfPrimeNumber();
-		
+		arrayListPrimeNumber.displaylistOfPrimeNumber(2, 100);
 	}
-
 }
