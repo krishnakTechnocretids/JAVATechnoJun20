61683ec076e34988b4d57d9571eee5f68266504b
Anup Sahoo <anupsahoo1408@gmail.com>
2020-07-27 19:46:18 -0400

    Anup_Assignment_34_Done_30mins

---
+++ src/anupSJun20/ReplaceWordwithaStringArrayList.java
@@ -0,0 +1,48 @@
+package anupSJun20;
+/*Program 2 :
+Display on which index Techno is available, Replace all Techno with Credits from given ArrayList, and count total number of Credits.
+["Maulik","Techno" ,"Krishna", "Techno", "Credits", "Techno"]
+
+Output : 
+Techno word index in given arrayList is 1,3,5 
+["Maulik","Credits" ,"Krishna", "Credits", "Credits", "Credits"]
+Total Credits words :- 4*/
+
+import java.util.ArrayList;
+
+public class ReplaceWordwithaStringArrayList {
+	
+	void replacewordwithArrayList(ArrayList<String> listofElements, String wordtoRemove, String wordtobereplaced) {
+		int count = 0;
+		System.out.print("The index of " + wordtoRemove + " is : ");
+		while(listofElements.contains(wordtoRemove)) {
+			if(listofElements.contains(wordtoRemove)) {
+				System.out.print(listofElements.indexOf(wordtoRemove) + " ");
+				listofElements.set(listofElements.indexOf(wordtoRemove), wordtobereplaced);
+			}
+		}
+		for(int index = 0;index<listofElements.size();index++) {
+			if(listofElements.get(index).equals(wordtobereplaced)) {
+				count++;
+			}
+		}
+		System.out.println("\nThe number of " + wordtobereplaced + " is " + count);
+		
+		
+	}
+	public static void main(String[] args) {
+		ReplaceWordwithaStringArrayList replaceWordwithaStringArrayList = new ReplaceWordwithaStringArrayList();
+		ArrayList<String> listofElements = new ArrayList<String>();
+		listofElements.add("Maulik");
+		listofElements.add("Techno");
+		listofElements.add("Krishna");
+		listofElements.add("Techno");
+		listofElements.add("Credits");
+		listofElements.add("Techno");
+		String wordtoRemove = "Techno";
+		String wordtobereplaced = "Credits";
+		System.out.println("The input arrayList is :" + listofElements);
+		replaceWordwithaStringArrayList.replacewordwithArrayList(listofElements, wordtoRemove, wordtobereplaced);
+	}
+
+}
