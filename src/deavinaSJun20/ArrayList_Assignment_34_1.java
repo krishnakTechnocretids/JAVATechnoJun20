c303f2d01d30d5604fcf4b26b8fa6d0b88c7a55d
Deavina Sharma <deavina.sharma@gmail.com>
2020-07-27 22:55:40 +0530

    Deavina_Assignment_Completed_10+20mins

---
+++ src/deavinaSJun20/ArrayList_Assignment_34_1.java
@@ -0,0 +1,48 @@
+package deavinaSJun20;
+
+/*
+ Program 2 :
+Display on which index Techno is available, Replace all Techno with Credits from given ArrayList, and count total number of Credits.
+["Maulik","Techno" ,"Krishna", "Techno", "Credits", "Techno"]
+
+Output : 
+Techno word index in given arrayList is 1,3,5 
+["Maulik","Credits" ,"Krishna", "Credits", "Credits", "Credits"]
+Total Credits words :- 4
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayList_Assignment_34_1 {
+	static int count = 0;
+
+	void removeandreplaceElements(ArrayList list) {
+
+		int indexofTechno = 0;
+		if (list.indexOf("Credits") != 0)
+			count++;
+		System.out.println("list of input elements : " + list);
+		System.out.print("Techno word in given arrayList is at index ");
+		for (int index = 0; index <= list.size(); index++) {
+
+			if (list.contains("Techno")) {
+				indexofTechno = list.indexOf("Techno");
+				System.out.print(indexofTechno + " ");
+				list.set(indexofTechno, "Credits");
+				count++;
+			}
+
+		}
+		System.out.println("\nList of updated element is : " + list);
+		System.out.println("Total Credits word count is :- " + count);
+
+	}
+
+	public static void main(String[] args) {
+		ArrayList listofinputelements = new ArrayList(
+				Arrays.asList("Maulik", "Techno", "Krishna", "Techno", "Credits", "Techno"));
+
+		new ArrayList_Assignment_34_1().removeandreplaceElements(listofinputelements);
+	}
+
+}
