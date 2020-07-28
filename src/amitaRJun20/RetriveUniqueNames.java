2307895d7db97aaed0f4ecdbd99957b07934e649
Amita Rout <amitarout11@gmail.com>
2020-07-28 11:33:57 -0400

    AMITA_Assignment_35_Done_10mins

---
+++ src/amitaRJun20/RetriveUniqueNames.java
@@ -0,0 +1,27 @@
+/*Assignment 35:  Retrieve all unique names from given array.
+String[] name1 = {"Palak", "Viresh", "Yash", "Aavruti"};
+String[] name2 = {"Deavina","Palak","Viresh", "Nikhil"};
+output : [Palak, Viresh, Yash, Aavruti,Deavina,Nikhil]
+Note : use collection concepts here and raise PR. For more practice write a code in notepad without using collection.*/
+package amitaRJun20;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RetriveUniqueNames {
+	
+	void retrieveNames(ArrayList<String> namelist1, ArrayList<String> namelist2) {
+		namelist2.removeAll(namelist1);
+		namelist1.addAll(namelist2);
+		System.out.println("\nThe final list after removing unique elements from Two arrays is :- \n" + namelist1);
+	}
+	public static void main(String[] args) {
+		String[] name1 = {"Palak", "Viresh", "Yash", "Aavruti"};
+		String[] name2 = {"Deavina","Palak","Viresh", "Nikhil"};
+		ArrayList<String> namelist1 = new ArrayList<String>(Arrays.asList(name1));
+		ArrayList<String> namelist2 = new ArrayList<String>(Arrays.asList(name2));
+		System.out.println("ArrayList name lis1 :- " + namelist1);
+		System.out.println("ArrayList name lis2 :- " + namelist2);
+		new RetriveUniqueNames().retrieveNames(namelist1, namelist2);
+	}
+}
