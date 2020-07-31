65128c84fcf5b450ff2ef04b023eb48d5e69a4be
“ADT” <aditi.gajipara@gmail.com>
2020-07-31 00:16:34 -0700

    ADT_Assignment_36_Done_17Mins

---
+++ src/aditiGJUN20/RemoveDuplicateElements.java
@@ -0,0 +1,47 @@
+/*
+ * Remove duplicate names from given String Array, print names in given format with its length. Input Order has to be maintained.
+ * String input[] = {"Aashtha","Kartikey","Piyush","Raj","Suparna","Piyush","Aashtha", "Barkha"}
+ * Output : 
+ * Aashtha -> 7
+ * Kartikey -> 8
+ * Piyush -> 6
+ * Raj -> 3
+ * Suparna -> 7
+ * Barkha -> 6
+ * Total unique elements are :- 6
+*/
+
+package aditiGJUN20;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicateElements {
+	void printUniqueList(String[] inputArray) {
+		LinkedHashSet<String> uniqueNameList = new LinkedHashSet<String>(removeDuplicates(inputArray));
+		Iterator<String> iterateName = uniqueNameList.iterator();
+		System.out.println("\n--------------------"+"\nUnique String elements for given String Array: ");
+		while (iterateName.hasNext()) {
+			String name = iterateName.next();
+			System.out.println(name + " -> " + name.length());
+		}
+	}
+
+	LinkedHashSet<String> removeDuplicates(String[] inputArray) {
+		LinkedHashSet<String> uniqueNameList = new LinkedHashSet<String>();
+		for (String name : inputArray) {
+			uniqueNameList.add(name);
+		}
+		return uniqueNameList;
+	}
+
+	public static void main(String[] args) {
+		RemoveDuplicateElements removeDuplicateElements = new RemoveDuplicateElements();
+		String input[] = { "Aashtha", "Kartikey", "Piyush", "Raj", "Suparna", "Piyush", "Aashtha", "Barkha" };
+		System.out.println("Input: " + Arrays.toString(input));
+		removeDuplicateElements.printUniqueList(input);
+		System.out.println("Total Unique Elements are: "+removeDuplicateElements.removeDuplicates(input).size());
+	}
+}
