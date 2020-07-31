8953f7204ad4dc4d13f2e091478dbb0cd6fe1b07
Archana Nawale <nawalearchana0@gmail.com>
2020-07-31 23:08:29 +0530

    Archana_Assignment_36_Updated

--- src/archanaNJun20/RemoveDuplicateNames.java
+++ src/archanaNJun20/RemoveDuplicateNames.java
@@ -1,19 +1,19 @@
 package archanaNJun20;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class RemoveDuplicateNames {
 	void removeDuplicateNamesFromString(String[] input) {
-		Set<String> nameString = new LinkedHashSet<>();
-		for(String name:input) 
-			nameString.add(name);
+		Set<String> nameString = new LinkedHashSet<>(Arrays.asList(input));
 		Iterator<String> iterator = nameString.iterator();
 		while (iterator.hasNext()) {
 			String temp=iterator.next();
 			System.out.println(temp+"->"+temp.length());
 		}
+		System.out.println("Total unique elements are :-" + nameString.size());
 	}
 	public static void main(String[] args) {
 		String input[] = { "Aashtha", "Kartikey", "Piyush", "Raj", "Suparna", "Piyush", "Aashtha", "Barkha" };
