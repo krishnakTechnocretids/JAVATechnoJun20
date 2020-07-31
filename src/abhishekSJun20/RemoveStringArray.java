ef70ee0b40daa4e1172ac473e557372b691b570f
“Abhishek <Abhishek.satyam10@gmail.com>
2020-07-31 22:18:22 +0530

    Abhishek_Assignment_36_25minute

---
+++ src/abhishekSJun20/RemoveStringArray.java
@@ -0,0 +1,35 @@
+package abhishekSJun20;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/*Remove duplicate names from given String Array, print names in given format with its length. Input Order has to be maintained.
+String input[] = {"Aashtha","Kartikey","Piyush","Raj","Suparna","Piyush","Aashtha", "Barkha"}
+Output : Aashtha -> 7
+              Kartikey -> 8
+              Piyush -> 6
+	      Raj -> 3
+	      Suparna -> 7
+	      Barkha -> 6
+Total unique elements are :- 6
+Note: Use set interface. */
+
+public class RemoveStringArray {
+	void removeDuplictae(String input[]) {
+		Set<String> setArray = new LinkedHashSet<>(Arrays.asList(input));
+		System.out.println("String Input:" + setArray);
+
+		System.out.print("Output : ");
+		for (String name : setArray) {
+			System.out.println(name + " -> " + name.length());
+		}
+		System.out.println("Total unique elements are :- " + setArray.size());
+	}
+
+	public static void main(String[] args) {
+		String input[] = { "Aashtha", "Kartikey", "Piyush", "Raj", "Suparna", "Piyush", "Aashtha", "Barkha" };
+		RemoveStringArray removeStringArray = new RemoveStringArray();
+		removeStringArray.removeDuplictae(input);
+	}
+}
