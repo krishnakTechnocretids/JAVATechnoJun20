2d6d0db32778a794eae4953dc7d7b06e3374462b
Deavina Sharma <deavina.sharma@gmail.com>
2020-08-02 10:58:23 +0530

    Deavina_Assignment35_

--- src/deavinaSJun20/ArrayListUnion.java
+++ src/deavinaSJun20/ArrayListUnion.java
@@ -13,25 +13,25 @@ output : [Palak, Viresh, Yash, Aavruti,Deavina,Nikhil]
 public class ArrayListUnion {
 
 	void uniqueName(ArrayList<String> arraylist1, ArrayList<String> arraylist2) {
-		for(String Name : arraylist2){
-			if(!arraylist1.contains(Name))
+		for (String Name : arraylist2) {
+			if (!arraylist1.contains(Name))
 				arraylist1.add(Name);
 		}
-		
+
 		System.out.println("\nOutput ArrayList : " + arraylist1);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] name1 = {"Palak", "Viresh", "Yash", "Aavruti"};
-		String[] name2 = {"Deavina","Palak","Viresh", "Nikhil"};
+		String[] name1 = { "Palak", "Viresh", "Yash", "Aavruti" };
+		String[] name2 = { "Deavina", "Palak", "Viresh", "Nikhil" };
 		ArrayList<String> nameList1 = new ArrayList<String>(Arrays.asList(name1));
 		System.out.println("Input ArrayList1 " + nameList1);
-		
+
 		ArrayList<String> nameList2 = new ArrayList<String>(Arrays.asList(name2));
 		System.out.println("Input ArrayList2 " + nameList2);
-		
-		new ArrayListUnion().uniqueName(nameList1,nameList2);
+
+		new ArrayListUnion().uniqueName(nameList1, nameList2);
 	}
 
 }
