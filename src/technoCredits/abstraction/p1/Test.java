c1b7848c9fb73aeba8e9111d7d26609f787195f5
Maulik Kanani <mkanani@rccl.com>
2020-08-03 19:26:49 +0530

    File

--- src/technoCredits/abstraction/p1/Test.java
+++ src/technoCredits/abstraction/p1/Test.java
@@ -13,6 +13,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		new Test(); // 1 // 7
-		System.out.println("Krishna");
+		System.out.println("Maulik");
+		System.out.println("krishna");
 	}
 }
