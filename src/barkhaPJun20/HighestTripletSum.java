31a65c5a77938c3b5204b363b2636ca881224455
Barkha Patle <barkhapatle01@gmail.com>
2020-09-24 08:44:03 +0530

    Minor changes

--- src/barkhaPJun20/HighestTripletSum.java
+++ src/barkhaPJun20/HighestTripletSum.java
@@ -6,6 +6,7 @@ package barkhaPJun20;
 public class HighestTripletSum {
 	int highestSum = 0;
 	int sum;
+	String triplets;
 
 	public void SumOfTriplet(int[] num) {
 		for (int index = 0; index < num.length; index++) {
@@ -13,10 +14,12 @@ public class HighestTripletSum {
 
 			if (sum > highestSum) {
 				highestSum = sum;
+				triplets=num[index]+", " + num[index + 1]+", " + num[index + 2];
 			}
 			index += 2;
 		}
 		System.out.println("Highest sum of Triplet is: " + highestSum);
+		System.out.println("Triplet set having highest sum is: "+triplets);
 	}	
 	public static void main(String[] args) {
 		HighestTripletSum highestTripletSum = new HighestTripletSum();
